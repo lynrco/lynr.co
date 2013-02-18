97db5647868eed608dfd4ab82849712f18caa963
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-18 17:50:15 -0500

    Add Image specs

---
+++ spec/quicklist/model/image_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec/autorun'
+require_relative '../../../lib/quicklist/model/image'
+
+describe Quicklist::Model::Image do
+
+  before(:all) do
+    @url = "//quicklist.it/assets/image.gif"
+    @image = Quicklist::Model::Image.new(300, 150, @url)
+  end
+
+  it "converts strings to integers when created" do
+    image = Quicklist::Model::Image.new("300", "150", @url)
+    image.width.should be(300)
+    image.width.should_not be("300")
+    image.height.should be(150)
+    image.height.should_not be("150")
+    image.should == @image
+  end
+
+  it "has a view with necessary data" do
+    view = @image.view
+    view.keys.should include(:width, :height, :url)
+    view.values.should include(300, 150, @url)
+  end
+
+end
