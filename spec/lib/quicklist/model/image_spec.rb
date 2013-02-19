67aaecf914de2070418f5172577f6dc324730139
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-19 11:57:48 -0500

    Refactor Image specs to have fewer expectations per spec

--- spec/lib/quicklist/model/image_spec.rb
+++ spec/lib/quicklist/model/image_spec.rb
@@ -8,19 +8,44 @@ describe Quicklist::Model::Image do
     @image = Quicklist::Model::Image.new(300, 150, @url)
   end
 
-  it "converts strings to integers when created" do
-    image = Quicklist::Model::Image.new("300", "150", @url)
-    image.width.should be(300)
-    image.width.should_not be("300")
-    image.height.should be(150)
-    image.height.should_not be("150")
-    image.should == @image
+  describe "#initialize" do
+
+    let(:image) { Quicklist::Model::Image.new("300", "150", @url) }
+
+    it "converts width to integer" do
+      image.width.should be(300)
+      image.width.should_not be("300")
+    end
+
+    it "converts height to integer" do
+      image.height.should be(150)
+      image.height.should_not be("150")
+    end
+
+    it "converts strings to integers" do
+      image.should == @image
+    end
+
   end
 
-  it "has a view with necessary data" do
-    view = @image.view
-    view.keys.should include(:width, :height, :url)
-    view.values.should include(300, 150, @url)
+  describe "#view" do
+
+    let(:view) { @image.view }
+
+    it "has the right keys" do
+      view.keys.should include(:width, :height, :url)
+    end
+
+    it "has the right values" do
+      view.values.should include(300, 150, @url)
+    end
+
+    it "has keys matching values" do
+      expect(view[:width]).to eq(300)
+      expect(view[:height]).to eq(150)
+      expect(view[:url]).to eq(@url)
+    end
+
   end
 
 end
