e817de0ae6829e0c02f0bd8a29cf7b6cebdd291b
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-19 22:18:07 -0500

    First pass at MongoDao implementation and spec

---
+++ lib/quicklist/persist/mongo_dao.rb
@@ -0,0 +1,46 @@
+require 'yaml'
+require 'mongo'
+
+module Quicklist; module Persist;
+
+  class MongoDao
+
+    attr_reader :config
+
+    def initialize(collection='default')
+      environment = ENV['whereami'] || 'development'
+      @config = YAML.load_file("config/database.#{environment}.yaml")['mongo']
+      @client = Mongo::MongoClient.new(@config['host'], @config['port'])
+      @db = @client.db(@config['database'])
+      if (@config['user'] && @config['pass'])
+        @db.authenticate(@config['user'], @config['pass'])
+      end
+      @coll = @db.collection(collection)
+    end
+
+    def save(obj)
+      id = obj['id'] || obj[:id]
+      result = obj.dup
+      if (id)
+        update(id, obj)
+      else
+        id = create(obj)
+        result[:id] = id
+      end
+      result
+    end
+
+    private
+
+    def create(record)
+      @coll.insert(record, { j: true })
+    end
+
+    def update(id, obj)
+      record = obj.reject { |k, v| k == 'id' || k == :id }
+      @coll.update({ _id: id }, record, { j: true })
+    end
+
+  end
+
+end; end;
---
+++ spec/lib/quicklist/persist/mongo_dao_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec/autorun'
+require 'yaml'
+require './lib/quicklist/persist/mongo_dao'
+
+describe Quicklist::Persist::MongoDao do
+
+  before(:each) do
+    ENV['whereami'] = 'test'
+    @config = YAML.load_file("config/database.#{ENV['whereami']}.yaml")
+  end
+
+  let(:dao) { Quicklist::Persist::MongoDao.new(collection='dummy') }
+
+  describe "#initialize" do
+
+    context "unconfigured environment" do
+
+      it "raises an Error in an unknown environment" do
+        ENV['whereami'] = 'neverland'
+        expect { Quicklist::Persist::MongoDao.new }.to raise_error(Errno::ENOENT)
+      end
+
+    end
+
+    context "configured environment" do
+
+      it "has a config property" do
+        expect(dao.config).not_to eq(nil)
+      end
+
+    end
+
+  end
+
+  describe "#config" do
+
+    it "has a host" do
+      expect(dao.config['host']).to eq(@config['mongo']['host'])
+    end
+
+    it "has a port" do
+      expect(dao.config['port']).to eq(@config['mongo']['port'])
+    end
+
+    it "has a database" do
+      expect(dao.config['database']).to eq(@config['mongo']['database'])
+    end
+
+  end
+
+  describe "#save" do
+
+    let(:record) { { price: 13532 } }
+
+    before(:each) do
+      # This requires a little inside baseball on what MongoDao does
+      # calls to save use create to actually hit the database if there
+      # isn't already an id field
+      # Note: this is kind of cheating
+      dao.stub(:create) do |record|
+        record[:id] = 789
+        record
+      end
+    end
+
+    it "gives an object the id property" do
+      expect(dao.save(record)[:id]).not_to eq(nil)
+    end
+
+  end
+
+end
