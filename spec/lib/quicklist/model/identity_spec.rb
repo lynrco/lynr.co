b97c7746ecd92a81d5c14a022748ffeafa06f5a6
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-20 12:33:08 -0500

    Organize identity specs into contexts

--- spec/lib/quicklist/model/identity_spec.rb
+++ spec/lib/quicklist/model/identity_spec.rb
@@ -10,29 +10,45 @@ describe Quicklist::Model::Identity do
     @valid = { email: @email, password: @password }
   end
 
-  it "can be compared to a Hash with email/password" do
-    (@ident == @valid).should be_true
+  describe "#==" do
+
+    it "compares to a Hash with email/password" do
+      (@ident == @valid).should be_true
+    end
+
   end
 
-  it "has an auth? method to check email/password" do
-    @ident.auth?(@valid[:email], @valid[:password]).should be_true
+  describe "#auth?" do
+
+    it "has an auth? method to check email/password" do
+      @ident.auth?(@valid[:email], @valid[:password]).should be_true
+    end
+
+    it "fails auth? on invalid password" do
+      @ident.auth?(@valid[:email], 'this is not my password').should be_false
+    end
+
   end
 
-  it "fails auth? on invalid password" do
-    @ident.auth?(@valid[:email], 'this is not my password').should be_false
+  describe "#view" do
+
+    it "has a view of it's data properties" do
+      view = @ident.view
+      view.keys.should include(:email, :password)
+      view.values.should include(@email)
+    end
+
   end
 
-  it "has a view of it's data properties" do
-    view = @ident.view
-    view.keys.should include(:email, :password)
-    view.values.should include(@email)
-  end
+  describe "#initialize" do
 
-  it "can be created from an existing password hash" do
-    pass = BCrypt::Password.create('this is a fake password')
-    tmp_ident = Quicklist::Model::Identity.new(@email, pass)
-    (tmp_ident == @valid).should be_true
-    tmp_ident.auth?(@valid[:email], 'invalid password').should be_false
+    it "can be created from an existing password hash" do
+      pass = BCrypt::Password.create('this is a fake password')
+      tmp_ident = Quicklist::Model::Identity.new(@email, pass)
+      (tmp_ident == @valid).should be_true
+      tmp_ident.auth?(@valid[:email], 'invalid password').should be_false
+    end
+
   end
 
 end
