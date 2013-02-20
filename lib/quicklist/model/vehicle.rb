1895e6f1967fa742322f1115e3539b4cf468f2d8
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-19 22:17:03 -0500

    Add some documentation to vehicle

--- lib/quicklist/model/vehicle.rb
+++ lib/quicklist/model/vehicle.rb
@@ -1,5 +1,29 @@
 module Quicklist; module Model;
 
+  # This is the primary object of the Application. Most of the data retrieval
+  # will result in Vehicle objects.
+  #
+  # `Vehicle.new` takes a `Hash` containing specific data properties to set.
+  #
+  # * `:year`, `String` car year
+  #
+  # * `:make`, `String` car make
+  #
+  # * `:model`, `String` car model
+  #
+  # * `:price`, an integer price in whole dollars (USD)
+  #
+  # * `:condition`, an integer from 0-5, inclusive, representing condition of the
+  #   vehicle. 0 indicates no rating
+  #
+  # * `:mpg`, a `Quicklist::Model::Mpg` object containing information about highway
+  #   and city mileage information.
+  #
+  # * `:vin`, a `Quicklist::Model::Vin` object containing all the information
+  #   that would be retrieved with a vin lookup. The object may or may not contain
+  #   the actual vin number.
+  #
+  # * `:images`, an `Array` of `Quicklist::Model::Image` objects.
   class Vehicle
 
     attr_reader :id
