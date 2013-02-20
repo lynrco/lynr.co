209daddb749d94860f3efd2dbf55345d652cfcc2
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-20 12:24:23 -0500

    Lazily establish connection to Mongo

--- lib/quicklist/persist/mongo_dao.rb
+++ lib/quicklist/persist/mongo_dao.rb
@@ -10,14 +10,33 @@ module Quicklist; module Persist;
     def initialize(collection='default')
       environment = ENV['whereami'] || 'development'
       @config = YAML.load_file("config/database.#{environment}.yaml")['mongo']
-      @client = Mongo::MongoClient.new(@config['host'], @config['port'])
-      @db = @client.db(@config['database'])
-      if (@config['user'] && @config['pass'])
-        @db.authenticate(@config['user'], @config['pass'])
-      end
-      @coll = @db.collection(collection)
+      @collection_name = collection
     end
 
+    # ## Manage the connection
+    #
+    def client
+      @client = Mongo::MongoClient.new(@config['host'], @config['port']) if @client == nil
+      @client
+    end
+
+    def db
+      if (@db == nil)
+        @db = client.db(@config['database'])
+        if (@config['user'] && @config['pass'])
+          @db.authenticate(@config['user'], @config['pass'])
+        end
+      end
+      @db
+    end
+
+    def collection
+      @collection = db.collection(@collection_name) if @coll == nil
+      @collection
+    end
+
+    # ## Operate on the collection
+    #
     def get(id)
       read(id)
     end
@@ -37,30 +56,30 @@ module Quicklist; module Persist;
       options = { }
       options[:skip] = skip if skip.is_a? Numeric
       options[:limit] = limit if limit.is_a? Numeric
-      @coll.find(query, options)
+      collection.find(query, options)
     end
 
-    protected
-
+    # ## CRUD
+    #
     # Returns the `_id` value for the new record
     def create(record)
-      @coll.insert(record, { j: true })
+      collection.insert(record, { j: true })
     end
 
     # Returns the record
     def read(id)
-      @coll.find_one({ _id: id })
+      collection.find_one({ _id: id })
     end
 
     # Returns `true` or the last error
     def update(id, obj)
       record = obj.reject { |k, v| k == 'id' || k == :id }
-      @coll.update({ _id: id }, record, { j: true })
+      collection.update({ _id: id }, record, { j: true })
     end
 
     # Returns `true` or the last error
     def delete(id)
-      @coll.remove({ _id: id }, { j: true })
+      collection.remove({ _id: id }, { j: true })
     end
 
   end
