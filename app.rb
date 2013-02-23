1142f21c122fd9497cf16ddfb3a6d5569b244822
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-23 15:18:15 -0500

    Add some error handling and controller implementation notes

--- app.rb
+++ app.rb
@@ -15,6 +15,15 @@ module Quicklist
 
     enable :logging
 
+    # taken from https://groups.google.com/d/msg/sinatrarb/lwd419mimJA/aptlwC5QJG4J
+    #error do
+    #  e = @env['sinatra.error']
+    #  [ halt 500, { 'Content-Type' => 'text/plain' }, "Internal Server Error - #{e.message}\n"
+    #end
+
+    # For request logging write Rack middleware that replaces env['rack.errors'] and env['rack.logger']
+    # This will likely require `enable :logging` to have it work
+
     set :public_folder, File.dirname(__FILE__) + '/public'
     set :views, File.dirname(__FILE__) + '/views'
 
@@ -23,6 +32,7 @@ module Quicklist
       erb :index
     end
 
+    # Call `use Quicklist::ControllerName` to use routes from other class definitions
 
   end
 
