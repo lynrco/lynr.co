f11ca48aa2da193ec1c672066eece09f6df5240c
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-23 15:13:46 -0500

    Logging module to be mixed in with things that do logging

---
+++ lib/quicklist/logging.rb
@@ -0,0 +1,27 @@
+require 'log4r'
+
+module Quicklist
+
+  # From https://gist.github.com/mindscratch/1145954
+  module Logging
+
+    FORMATTER = Log4r::PatternFormatter.new(:pattern => '[%d] %l %C: %M')
+   
+    OPTS = { formatter: FORMATTER }
+    
+    # Get an instance to a logger configured for the class that includes it.
+    # This allows log messages to include the class name
+    def log
+      return @logger if @logger
+      
+      @logger = Log4r::Logger.new(self.class.name)
+      @logger.outputters << Log4r::StdoutOutputter.new("console", OPTS)
+      if (ENV['logfile'])
+        @logger.outputters << Log4r::FileOutputter.new("file", OPTS.merge({ filename: ENV['logfile'] }))
+      end
+   
+      @logger
+     end
+  end
+
+end
