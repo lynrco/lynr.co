03c3e0c37b397831b28350ddac4dc3c3ed9188df
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-26 20:16:31 -0500

    Remove the helper methods and add before and after filters

--- lib/lynr/controller/base.rb
+++ lib/lynr/controller/base.rb
@@ -7,7 +7,15 @@ module Lynr; module Controller;
     include Lynr::Logging
 
     engine :erb
-    helper :xhtml
+
+    before_all do
+      log.info "Request  - '#{request.path_info}'"
+    end
+
+    after_all do
+      log.info "Response - '#{request.path_info}' -- #{response.status}"
+      response['Server'] = 'Lynr.co Application Server'
+    end
 
   end
 
