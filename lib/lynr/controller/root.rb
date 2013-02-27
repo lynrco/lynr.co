c88d322f195141b9b24f1d5403fe3dc2c9abd342
Bryan J Swift <bryan.j.swift@gmail.com>
2013-02-26 20:16:52 -0500

    Add some Controller::Root documentation

--- lib/lynr/controller/root.rb
+++ lib/lynr/controller/root.rb
@@ -2,12 +2,20 @@ require './lib/lynr/controller/base'
 
 module Lynr; module Controller;
 
+  # # `Lynr::Controller::Root`
+  #
+  # `Root` is the catch all controller. It will handle everything
+  # not mapped elsewhere in the project.
   class Root < Lynr::Controller::Base
 
     map '/'
 
     layout :mobile_default
 
+    # ## `Lynr::Controller::Root#index`
+    #
+    # Homepage. Automagically renders `views/index.erb` based on the method name
+    # servicing the request, in this case `index`
     def index
       @title = "Hi there!"
     end
