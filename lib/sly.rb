307842fc3d46476db3af5ebff7d9ada337aa3d73
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-03 17:09:00 -0500

    Add some functionality to Sly in the form of an App class

--- lib/sly.rb
+++ lib/sly.rb
@@ -1,2 +1,29 @@
+require 'rack'
+require 'innate'
+
+require './lib/sly/node'
+require './lib/sly/route'
+
 module Sly
+
+  DynaMap = Innate::URLMap.new
+  Cascade = Rack::Cascade.new([])
+
+  class App
+
+    def self.add(path, route)
+      Sly::DynaMap.map(path, route)
+      Sly::Cascade.add(route)
+    end
+
+    def initialize(apps)
+      @apps = apps
+    end
+
+    def call(env)
+      Sly::DynaMap.call(env)
+    end
+
+  end
+
 end
