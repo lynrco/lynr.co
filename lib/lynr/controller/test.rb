aa11c62835f474ffce31bc6d54ca964782bd8b38
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-03 17:50:58 -0500

    Add another route to Controller::Test

--- lib/lynr/controller/test.rb
+++ lib/lynr/controller/test.rb
@@ -1,11 +1,23 @@
 require './lib/sly'
 require 'rack'
 
-class Test < Sly::Node
+module Lynr; module Controller;
 
-  get '/test', :index
-  def index(req)
-    Rack::Response.new(["hi"], 200, { "Content-Type" => "text/plain" }).finish
+  class Test < Sly::Node
+
+    include Lynr::Logging
+
+    get '/test', :index
+    def index(req)
+      Rack::Response.new(["hi"], 200, { "Content-Type" => "text/plain" }).finish
+    end
+
+    get '/car', :vehicle
+    def vehicle(req)
+      log.info({ path_info: req.path })
+      Rack::Response.new(["hi car"], 200, { "Content-Type" => "text/plain" }).finish
+    end
+
   end
 
-end
+end; end;
