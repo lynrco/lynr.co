d266779315bf9abd529cb74dce9168b5bfc289b1
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-03 15:52:44 -0500

    Flesh out the map method and add some convenience methods for controllers

--- lib/sly/controller.rb
+++ lib/sly/controller.rb
@@ -1,8 +1,32 @@
+require 'rack'
+
+require './lib/sly/route'
+
 module Sly
 
   class Controller
 
-    def self.map(route, method_name, verb='GET')
+    ##
+    # Mapped methods must return an Array with the form
+    # `[status_code (int), headers (Hash), body (Iterable)]`. This may be
+    # recognized as the result of `Rack::Response#finish`
+    #
+    def self.map(path, method_name, verb='GET')
+      if method_name.is_a? Sly::Route
+        route = method_name
+        DynaMap.map(path, route)
+      else
+        method = method_name.to_sym
+        DynaMap.map(path, Route.new(verb, path, lambda { |req| self.new.send(method, req) }))
+      end
+    end
+
+    def error(code = 500)
+      Rack::Response.new(status = code)
+    end
+
+    def not_found
+      error(404)
     end
 
   end
