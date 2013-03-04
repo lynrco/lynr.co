60023e08184a8e5b39ed40b7177e704324958257
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-03 21:27:59 -0500

    Use the new Sly::URLMap class

--- lib/sly.rb
+++ lib/sly.rb
@@ -3,10 +3,11 @@ require 'innate'
 
 require './lib/sly/node'
 require './lib/sly/route'
+require './lib/sly/urlmap'
 
 module Sly
 
-  DynaMap = Innate::URLMap.new
+  DynaMap = Sly::URLMap.new
   Cascade = Rack::Cascade.new([])
 
   class App
@@ -21,7 +22,9 @@ module Sly
     end
 
     def call(env)
-      res = Sly::Cascade.call(env)
+      res = Sly::DynaMap.call(env)
+      # Behave like a cascade
+      # TODO: Make this an option
       if (res[0].to_i == 404)
         @app.call(env)
       else
