d8f68338ee8cc0358e8323e3ecbee3b6e59852a4
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-04 02:06:15 -0500

    Add a second handler and define routes at the top

--- lib/lynr/controller/test.rb
+++ lib/lynr/controller/test.rb
@@ -7,15 +7,24 @@ module Lynr; module Controller;
 
     include Lynr::Logging
 
+    # Order of route mapping matters because they are hit in order.
+    # So the most general one must be specified last
+    get '/car/:id', :vehicle2
+    get '/car', :vehicle
     get '/test', :index
+
     def index(req)
       Rack::Response.new(["hi"], 200, { "Content-Type" => "text/plain" }).finish
     end
 
-    get '/car', :vehicle
     def vehicle(req)
       log.info({ path_info: req.path })
       Rack::Response.new(["hi car"], 200, { "Content-Type" => "text/plain" }).finish
+    end
+
+    def vehicle2(req)
+      log.info({ path_info: req.path })
+      Rack::Response.new(["hi car2"], 200, { "Content-Type" => "text/plain" }).finish
     end
 
   end
