2b9b1901c0db2a3d51091672a67a099c97db808a
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-20 04:21:28 -0400

    Add Sly::View::Erb to represent an ERB view to be rendered
    Defines a basic API for defining a view object and producing a string
    representation of it

---
+++ lib/sly/view/erb.rb
@@ -0,0 +1,32 @@
+module Sly; module View;
+
+  class Erb
+
+    def initialize(path, opts)
+      layout_path = opts[:layout] || nil
+      @layout = get_template(layout_path) if layout_path
+      @template = get_template(path) if path
+      @context = opts[:context] || nil
+    end
+
+    def result
+      require 'pry'
+      binding.pry
+      if (@layout)
+        @layout.result(@context.binding { @template.result(@context.binding) })
+      else
+        @template.result(@context.binding)
+      end
+    end
+
+    private
+
+    def get_template(path)
+      file_name = "#{path.to_s}.erb"
+      str = ::File.read(file_name)
+      ::ERB.new(str, nil, '%<>')
+    end
+
+  end
+
+end; end;
