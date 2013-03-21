f9ca679fae48f4096617984045540e1812cf22d7
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-21 19:29:13 -0400

    Basic handling for get and post requests to /signup resource

--- lib/lynr/controller/auth.rb
+++ lib/lynr/controller/auth.rb
@@ -1,17 +1,37 @@
-require './lib/sly'
 require 'rack'
+
+require './lib/sly'
+require './lib/sly/view/erb_helpers'
 
 module Lynr; module Controller;
 
   class Auth < Sly::Node
 
     include Lynr::Logging
+    # Provides `render` and `render_partial` methods
+    include Sly::View::ErbHelpers
 
-    get '/signup', :get_signup
+    def initialize
+      super
+      @headers = { "Content-Type" => "text/html; charset=utf-8" }
+      @section = "auth"
+    end
+
+    get  '/signup', :get_signup
+    post '/signup', :post_signup
 
     def get_signup(req)
+      @subsection = "signup"
+      @posted = {}
+      render 'auth/signup', :layout => 'default_sly'
+    end
+
+    def post_signup(req)
+      @subsection = "signup submitted"
+      @posted = {}
+      render 'auth/signup', :layout => 'default_sly'
     end
 
   end
 
-end
+end; end;
