db7697ef01caaeaae71acd83a92871251935d39f
Bryan J Swift <bryan.j.swift@gmail.com>
2013-03-31 22:08:20 -0400

    Add a timer middleware class to provide information about response times

---
+++ lib/rack/middleware/timer.rb
@@ -0,0 +1,27 @@
+require 'date'
+
+module Rack; module Middleware;
+
+  class Timer
+
+    def initialize(app, log)
+      @app = app
+      @log = log
+    end
+
+    def call(env)
+      start = Time.now
+      status, headers, @response = @app.call(env)
+      stop = Time.now
+      @log.info({ type: 'data', path: env['PATH_INFO'], status: status, elapsed: "#{stop - start} ms" })
+      headers['x-response-time'] = "#{stop - start} ms" if !headers.include? 'x-response-time'
+      [status, headers, self]
+    end
+
+    def each(&block)
+      @response.each(&block)
+    end
+
+  end
+
+end; end;
