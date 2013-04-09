14b18aa03d48241cb4cf3cdb52651a03ff6625b5
Bryan J Swift <bryan.j.swift@gmail.com>
2013-04-09 17:36:07 -0400

    Add method to list vehicles for a dealership

--- lib/lynr/persist/vehicle_dao.rb
+++ lib/lynr/persist/vehicle_dao.rb
@@ -5,9 +5,11 @@ module Lynr; module Persist;
 
   class VehicleDao
 
+    SORT = ['created_at', Mongo::DESCENDING]
+
     def initialize
       @dao = MongoDao.new('collection' => 'vehicles')
-      #@dao.collection.ensure_index([['identity.email', Mongo::ASCENDING]], { unique: true })
+      @dao.collection.ensure_index([['dealership', Mongo::ASCENDING], SORT])
     end
 
     def get(id)
@@ -15,6 +17,16 @@ module Lynr; module Persist;
       # Mongo is going to give me a record with the _id property set, not id
       record['id'] = record.delete('_id') if !record.nil?
       Lynr::Model::Vehicle.inflate(record)
+    end
+
+    def list(dealership, page=1, count=10)
+      skip = (page - 1) * count
+      options = { skip: skip, limit: count, sort: SORT }
+      records = @dao.search({ 'dealership' => dealership.id }, options)
+      records.map do |record|
+        dealership_id = record.delete('dealership')
+        Lynr::Model::Vehicle.inflate(record)
+      end
     end
 
     def save(vehicle)
--- spec/lib/lynr/persist/vehicle_dao_spec.rb
+++ spec/lib/lynr/persist/vehicle_dao_spec.rb
@@ -1,7 +1,6 @@
 require 'rspec/autorun'
 require './spec/spec_helper'
 
-require 'yaml'
 require './lib/lynr/persist/vehicle_dao'
 
 require './lib/lynr/model/image'
@@ -12,21 +11,29 @@ describe Lynr::Persist::VehicleDao do
 
   let(:dao) { Lynr::Persist::VehicleDao.new }
 
+  let(:dealership_id) { BSON::ObjectId.new }
+  let(:dealership) { Lynr::Model::Dealership.new({}, dealership_id) }
+
   let(:image) { Lynr::Model::Image.new("300", "150", "//lynr.co/assets/image.gif") }
   let(:mpg) { Lynr::Model::Mpg.new({ 'city' => 28.8, 'highway' => 33.2 }) }
   let(:vin) { Lynr::Model::Vin.new("Manual", "28 L", "2", "AWD", "Silver", "Charcoal") }
   let(:vehicle_data) {
     {
-      'year'      => '2010',
-      'make'      => 'Mitsubishi',
-      'model'     => 'Gallant',
-      'price'     => 4999.99,
-      'condition' => 3,
-      'mpg'       => mpg,
-      'vin'       => vin,
-      'images'    => [image]
+      'year'       => '2010',
+      'make'       => 'Mitsubishi',
+      'model'      => 'Gallant',
+      'price'      => 4999.99,
+      'condition'  => 3,
+      'mpg'        => mpg,
+      'vin'        => vin,
+      'images'     => [image],
+      'dealership' => dealership
     }
   }
+
+  before(:each) do
+    dealership.stub(:id) { dealership_id }
+  end
 
   context "with active connection", :if => (MongoHelpers.connected?) do
     
@@ -58,7 +65,7 @@ describe Lynr::Persist::VehicleDao do
         expect(saved.vin).to eq(vehicle_data['vin'])
       end
 
-    end
+    end # #save
 
     describe "#get" do
 
@@ -87,7 +94,42 @@ describe Lynr::Persist::VehicleDao do
         expect(found.vin).to eq(saved.vin)
       end
 
-    end
+    end # #get
+
+    describe "#list" do
+
+      context "in DB with no records" do
+
+        it "returns an empty collection when nothing saved" do
+          expect(dao.list(dealership)).to be_empty
+        end
+
+      end
+
+      context "in DB with 1 record" do
+
+        before(:each) do
+          @list_saved = dao.save(vehicle)
+        end
+
+        it "returns a non-empty collection when something saved" do
+          expect(dao.list(dealership)).to_not be_empty
+        end
+
+        it "returns a collection of Vehicles" do
+          dao.list(dealership).each do |item|
+            expect(item).to be_an_instance_of(Lynr::Model::Vehicle)
+          end
+        end
+
+        it "includes the saved record" do
+          listed = dao.list(dealership)
+          expect(dao.list(dealership)).to include(@list_saved)
+        end
+
+      end
+
+    end # #list
 
   end
 
