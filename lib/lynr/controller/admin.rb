033d22ae452ee19a9371c4677f253e9cbe4505ab
Bryan J Swift <bryan.j.swift@gmail.com>
2013-04-09 17:44:28 -0400

    Get vehicles and add them to view data

--- lib/lynr/controller/admin.rb
+++ lib/lynr/controller/admin.rb
@@ -1,6 +1,7 @@
 require './lib/sly'
 require './lib/lynr/controller/base'
 require './lib/lynr/persist/dealership_dao'
+require './lib/lynr/persist/vehicle_dao'
 require './lib/lynr/validator/helpers'
 
 module Lynr; module Controller;
@@ -12,12 +13,13 @@ module Lynr; module Controller;
   #
   class Admin < Lynr::Controller::Base
 
-    attr_reader :dao
+    attr_reader :dealer_dao, :vehicle_dao
 
     def initialize
       super
       @section = "admin"
-      @dao = Lynr::Persist::DealershipDao.new
+      @dealer_dao = Lynr::Persist::DealershipDao.new
+      @vehicle_dao = Lynr::Persist::VehicleDao.new
     end
 
     get '/admin/:slug', :index
@@ -29,8 +31,9 @@ module Lynr; module Controller;
     def index(req)
       @subsection = 'index'
       id = BSON::ObjectId.from_string(req['slug'])
-      @dealership = dao.get(id)
+      @dealership = dealer_dao.get(id)
       return not_found if @dealership.nil?
+      @vehicles = vehicle_dao.list(@dealership)
       @title = "Welcome back #{@dealership.name}"
       @owner = @dealership.name
       render 'admin/index.erb'
