ee61b7581cd591fa755b0d907b7b713531035703
Bryan J Swift <bryan.j.swift@gmail.com>
2013-04-10 09:52:21 -0400

    Add the basics of an account information handler

--- lib/lynr/controller/admin.rb
+++ lib/lynr/controller/admin.rb
@@ -22,6 +22,7 @@ module Lynr; module Controller;
       @vehicle_dao = Lynr::Persist::VehicleDao.new
     end
 
+    get '/admin/:slug/account', :get_account
     get '/admin/:slug', :index
 
     # ## `Lynr::Controller::Admin#index`
@@ -29,7 +30,7 @@ module Lynr; module Controller;
     # Admin Homepage. Renders `views/admin/index.erb`.
     #
     def index(req)
-      @subsection = 'index'
+      @subsection = 'list'
       id = BSON::ObjectId.from_string(req['slug'])
       @dealership = dealer_dao.get(id)
       return not_found if @dealership.nil?
@@ -37,6 +38,49 @@ module Lynr; module Controller;
       @title = "Welcome back #{@dealership.name}"
       @owner = @dealership.name
       render 'admin/index.erb'
+    end
+
+    def get_account(req)
+      @subsection = 'account'
+      return not_found if !authorized?(req)
+      @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
+      render 'admin/account.erb'
+    end
+
+    # ## Helpers
+
+    # ## `Lynr::Controller::Admin#session_user`
+    #
+    # Gets the current user out of the session and returns it
+    #
+    # ### Params
+    #
+    # * `req` Request with access to session out of which to get the user
+    #
+    # ### Returns
+    #
+    # Currently logged in instance of `Lyrn::Model::Dealership`
+    #
+    def session_user(req)
+      id = req.session['dealer_id']
+      dealer_dao.get(id)
+    end
+
+    # ## `Lynr::Controller::Admin#authorized?`
+    #
+    # Whether or not the current user is authorized to access the requested dealership
+    #
+    # ### Params
+    #
+    # * `req` Request with session and dealership information to be compared
+    #
+    # ### Returns
+    #
+    # true if current user is allowed to view/modify the requested Dealership
+    # false otherwise
+    #
+    def authorized?(req)
+      req.session['dealer_id'] == BSON::ObjectId.from_string(req['slug'])
     end
 
   end
