0ae764ff3fdd6d0cdd3a1925648995dcf952ae6b
Bryan J Swift <bryan.j.swift@gmail.com>
2013-04-21 18:09:03 -0400

    Add billing controller so customers can update billing info

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require 'lynr/config'
 require 'lynr/controller/root'
 require 'lynr/controller/admin'
 require 'lynr/controller/admin/account'
+require 'lynr/controller/admin/billing'
 require 'lynr/controller/auth'
 
 module Lynr
---
+++ lib/lynr/controller/admin/billing.rb
@@ -0,0 +1,78 @@
+require 'lynr/controller/admin'
+
+module Lynr; module Controller;
+
+  class AdminBilling < Lynr::Controller::Admin
+
+    get  '/admin/:slug/billing', :get_billing
+    post '/admin/:slug/billing', :post_billing
+
+    def get_billing(req)
+      return unauthorized unless authorized?(req)
+      @subsection = 'billing'
+      @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
+      @title = "Billing Information"
+      @msg = req.session['billing_flash_msg']
+      render 'admin/billing.erb'
+    end
+
+    def post_billing(req)
+      return unauthorized unless authorized?(req)
+      @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
+      @posted = req.POST
+      @errors = validate_billing_info
+      if @errors.empty?
+        customer = Stripe::Customer.retrieve(@dealership.customer_id)
+        customer.card = posted['stripeToken']
+        customer.save
+        req.session['billing_flash_msg'] = "Card updated successfully."
+        redirect "/admin/#{@dealership.id.to_s}/billing"
+      else
+        render 'admin/billing.erb'
+      end
+    rescue Stripe::CardError => sce
+      handle_stripe_error!(sce, sce.message)
+    rescue Stripe::InvalidRequestError => sire
+      handle_stripe_error!(sire, "You might have submitted the form more than once.")
+    rescue Stripe::AuthenticationError, Stripe::APIConnectionError, Stripe::StripeError => sse
+      msg = "Couldn't communicate with our card processor. We've been notified of the error."
+      handle_stripe_error!(sse, msg)
+    end
+
+    # ## `Lynr::Controller::AdminBilling#handle_stripe_error!`
+    #
+    # This method takes an error and message and maps it to the credit card
+    # fields and then provides an appropriate response object. The 'bang' at
+    # the end of the method name signifies it terminates a request.
+    #
+    # ### Params
+    #
+    # * `err` is a Exception or Error class, it could be any kind of object
+    #   but it is logged as a warning.
+    # * `message` is the error message displayed to the potential customer
+    #   informing them of the problem. This message is tied to the credit card
+    #   info.
+    #
+    # ### Returns
+    #
+    # A `Rack::Response` style object that responds to a `finish` message.
+    #
+    def handle_stripe_error!(err, message)
+      log.warn { err }
+      @errors['stripeToken'] = message
+      render 'admin/billing.erb'
+    end
+
+    def validate_billing_info
+      errors = {}
+
+      if (posted['stripeToken'].nil? || posted['stripeToken'].empty?)
+        errors['stripeToken'] = "Your card wasn't accepted."
+      end
+
+      errors
+    end
+
+  end
+
+end; end;
