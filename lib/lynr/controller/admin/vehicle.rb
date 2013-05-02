83e5f7cb6cbf592c9ce714883eebce0ed0c99b79
Bryan J Swift <bryan.j.swift@gmail.com>
2013-05-02 13:37:11 -0400

    Updates to make creating vehicles possible

--- lib/lynr/controller/admin/vehicle.rb
+++ lib/lynr/controller/admin/vehicle.rb
@@ -1,10 +1,12 @@
 require 'lynr/controller/admin'
+require 'lynr/model/vehicle'
 
 module Lynr; module Controller;
 
   class AdminVehicle < Lynr::Controller::Admin
 
     get  '/admin/:slug/vehicle/add', :get_add
+    post '/admin/:slug/vehicle/add', :post_add
 
     def get_add(req)
       return unauthorized unless authorized?(req)
@@ -12,6 +14,15 @@ module Lynr; module Controller;
       @title = 'Add Vehicle'
       @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
       render 'admin/vehicle/add.erb'
+    end
+
+    def post_add(req)
+      return unauthorized unless authorized?(req)
+      dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
+      @posted = req.POST.dup
+      posted['dealership'] = @dealership
+      vehicle = vehicle_dao.save(Lynr::Model::Vehicle.inflate(posted))
+      redirect "/admin/#{dealership.slug}/#{vehicle.slug}"
     end
 
   end
