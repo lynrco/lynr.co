2c71f6fef7dff909fdede529bd044a4ab2dc965f
Bryan J Swift <bryan.j.swift@gmail.com>
2013-05-02 13:37:28 -0400

    Baseline for vehicle editing

--- lib/lynr/controller/admin/vehicle.rb
+++ lib/lynr/controller/admin/vehicle.rb
@@ -7,6 +7,7 @@ module Lynr; module Controller;
 
     get  '/admin/:slug/vehicle/add', :get_add
     post '/admin/:slug/vehicle/add', :post_add
+    get  '/admin/:slug/:vehicle',    :get_vehicle
 
     def get_add(req)
       return unauthorized unless authorized?(req)
@@ -23,6 +24,16 @@ module Lynr; module Controller;
       posted['dealership'] = @dealership
       vehicle = vehicle_dao.save(Lynr::Model::Vehicle.inflate(posted))
       redirect "/admin/#{dealership.slug}/#{vehicle.slug}"
+    end
+
+    def get_vehicle(req)
+      return unauthorized unless authorized?(req)
+      @subsection = 'vehicle vehicle-add'
+      @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
+      @vehicle = vehicle_dao.get(BSON::ObjectId.from_string(req['vehicle']))
+      @title = "Edit #{@vehicle.year} #{@vehicle.make} #{@vehicle.model}"
+      @posted = @vehicle.view
+      render 'admin/vehicle/edit.erb'
     end
 
   end
