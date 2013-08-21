3fe8c8ffef2aaa5ba35f82c87559bfe0fa0b9cc0
Bryan J Swift <bryan.j.swift@gmail.com>
2013-08-21 01:50:05 -0400

    Handle to_json for model objects including Base

--- lib/lynr/model/base.rb
+++ lib/lynr/model/base.rb
@@ -6,6 +6,10 @@ module Lynr; module Model;
       obj = (o.respond_to?(:view) && o.view) || o
       view == obj
     end
+
+    def to_json
+      view.to_json
+    end
     
     def view
       { }
