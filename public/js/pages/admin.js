9d76a619a28066f21e072e39f0762367d0c0d883
Bryan J Swift <bryan.j.swift@gmail.com>
2013-08-28 18:53:20 -0400

    Add in the spinner starting and stopping for image uploads

--- public/js/pages/admin.js
+++ public/js/pages/admin.js
@@ -8,13 +8,18 @@ define(function(require) {
 
   var baseTransloaditOpts = {
     autoSubmit: false,
+    fields: 'input[name=idx], input[name=dealership_id]',
+    modal: false,
     triggerUploadOnFileSelection: true,
+    onStart: uploadStart,
     wait: true
   };
 
+  var spinners = {};
+
   function initAccount() {
     require(
-      ['jquery', 'jquery.transloadit'],
+      ['jquery', 'jquery.transloadit', 'modules/spinner'],
       function($, jtl) {
         $('form.account-photo').transloadit(transloaditOpts('account'));
       }
@@ -39,10 +44,14 @@ define(function(require) {
 
   function initVehiclePhotos() {
     require(
-      ['jquery', 'jquery.transloadit'],
+      ['jquery', 'jquery.transloadit', 'modules/spinner'],
       function($, jtl) {
         var forms = $('.vehicle-photo');
-        forms.transloadit(transloaditOpts('vehicle-photos'));
+        var opts = transloaditOpts('vehicle-photos');
+        forms.each(function() {
+          var form = $(this);
+          form.transloadit(opts);
+        });
       }
     );
   }
@@ -57,10 +66,7 @@ define(function(require) {
         };
         break;
       case 'account':
-        specific = {
-          onSuccess: uploadAccountSuccess,
-          fields: 'input[name=dealership_id]'
-        };
+        specific = { onSuccess: uploadAccountSuccess };
         break;
       default:
         specific = {};
@@ -70,53 +76,75 @@ define(function(require) {
   }
 
   function uploadAccountSuccess(assembly) {
-    var results = assembly.results;
-    var input = $('input[name=image]');
-    var original = results[':original'][0];
-    var full = results.resize_full[0];
-    var thumb = results.resize_thumb[0];
-    var image = {
-      url: full.url,
-      src: full.url,
-      width: full.meta.width,
-      height: full.meta.height
-    };
-    input.val(JSON.stringify(image));
-    $('img.photo-preview').attr(image);
-  }
-
-  function uploadPhotosSuccess(assembly) {
-    var results = assembly.results;
-    var fields = assembly.fields;
-    var form = $('#photo-' + fields.idx);
-    var input = $('input[name=images]');
-    var original = results[':original'][0];
-    var full = results.resize_full[0];
-    var thumb = results.resize_thumb[0];
-    var image = {
-      original: {
-        url: original.url,
-        src: original.url,
-        width: original.meta.width,
-        height: original.meta.height
-      },
-      full: {
+    require(['jquery'], function($) {
+      uploadSuccessStopSpinner($, assembly);
+      var results = assembly.results;
+      var input = $('input[name=image]');
+      var original = results[':original'][0];
+      var full = results.resize_full[0];
+      var thumb = results.resize_thumb[0];
+      var image = {
         url: full.url,
         src: full.url,
         width: full.meta.width,
         height: full.meta.height
-      },
-      thumb: {
-        url: thumb.url,
-        src: thumb.url,
-        width: thumb.meta.width,
-        height: thumb.meta.height
-      }
-    };
-    var images = JSON.parse(input.val());
-    images[fields.idx] = image;
-    input.val(JSON.stringify(images));
-    form.find('img.photo-preview').attr(image.full);
+      };
+      input.val(JSON.stringify(image));
+      $('img.photo-preview').attr(image);
+    });
+  }
+
+  function uploadPhotosSuccess(assembly) {
+    require(['jquery'], function($) {
+      uploadSuccessStopSpinner($, assembly);
+      var results = assembly.results;
+      var fields = assembly.fields;
+      var form = $('#photo-' + fields.idx);
+      var input = $('input[name=images]');
+      var original = results[':original'][0];
+      var full = results.resize_full[0];
+      var thumb = results.resize_thumb[0];
+      var image = {
+        original: {
+          url: original.url,
+          src: original.url,
+          width: original.meta.width,
+          height: original.meta.height
+        },
+        full: {
+          url: full.url,
+          src: full.url,
+          width: full.meta.width,
+          height: full.meta.height
+        },
+        thumb: {
+          url: thumb.url,
+          src: thumb.url,
+          width: thumb.meta.width,
+          height: thumb.meta.height
+        }
+      };
+      var images = JSON.parse(input.val());
+      images[fields.idx] = image;
+      input.val(JSON.stringify(images));
+      form.find('img.photo-preview').attr(image.full);
+    });
+  }
+
+  function uploadStart(assembly) {
+    require(['jquery', 'modules/spinner'], function($, spinner) {
+      var fields = assembly.fields;
+      var form = $('#photo-' + fields.idx);
+      var spin = form.data('spinner') || spinner(form.find('.fs-photo')[0]);
+      form.data('spinner', spin);
+      spinners[fields.idx] = spin;
+    });
+  }
+
+  function uploadSuccessStopSpinner($, assembly) {
+    var fields = assembly.fields;
+    var form = $('#photo-' + fields.idx);
+    form.data('spinner').stop();
   }
 
   return api;
