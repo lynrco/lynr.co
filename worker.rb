100c972dd0da69200e951b585f390755fdba7bd8
Bryan J Swift <bryan.j.swift@gmail.com>
2013-09-11 21:53:19 -0400

    [WIP] Worker queue start

--- worker.rb
+++ worker.rb
@@ -1,6 +1,5 @@
 require 'bundler/setup'
-require 'amqp'
-require 'amqp/extensions/rabbitmq'
+require 'bunny'
 
 require 'lynr'
 require 'lynr/config'
@@ -9,6 +8,27 @@ require 'lynr/logging'
 module Lynr
 
   class Worker
+
+    @app = false
+
+    attr_reader :config
+
+    def initialize
+      @config = Lynr::Config.new('app', ENV['whereami'])
+    end
+
+    def self.instance
+      @app = Lynr::Web.new if !@app
+      @app
+    end
+
+    def self.config
+      instance.config
+    end
+
+    Producer = Bunny.new(Lynr::Worker.config['amqp']['producer'])
+    Consumer = Bunny.new(Lynr::Worker.config['amqp']['consumer'])
+
   end
 
 end
