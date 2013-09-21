ed948145ef6634d38de08cc94eb207e554eeec5d
Bryan J Swift <bryan.j.swift@gmail.com>
2013-09-21 17:33:44 -0400

    Add the base Job class and the JobResult class
    These will be useful in a Queue subclass for executing arbitrary tasks.

---
+++ lib/lynr/worker/job.rb
@@ -0,0 +1,28 @@
+require 'lynr/logging'
+require 'lynr/worker/job_result'
+
+module Lynr; module Worker;
+
+  class Job
+
+    include Lynr::Logging
+
+    Success = JobResult.new
+
+    def perform
+      Success
+    end
+
+    protected
+
+    def failure(message)
+      JobResult.new(message, false)
+    end
+
+    def success
+      JobResult.new
+    end
+
+  end
+
+end; end;
---
+++ lib/lynr/worker/job_result.rb
@@ -0,0 +1,27 @@
+module Lynr; module Worker;
+
+  class JobResult
+
+    attr_reader :message
+
+    def initialize(message = "", succeeded = true)
+      @message = message
+      @succeeded = succeeded
+    end
+
+    def succeeded?
+      @succeeded
+    end
+
+    def to_s
+      result = if @succeeded
+        'Success'
+      else
+        "Failure message=#{@message}"
+      end
+      "#<#{self.class.name}:#{object_id} #{result}>"
+    end
+
+  end
+
+end; end;
