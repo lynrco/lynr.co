fa3679817ec50a636f65a8f73559395ceeb18538
Bryan J Swift <bryan.j.swift@gmail.com>
2013-11-01 19:55:56 -0400

    Remove inflate_xml and associated specs from `Model::Vin`

--- lib/lynr/model/vin.rb
+++ lib/lynr/model/vin.rb
@@ -48,23 +48,6 @@ module Lynr; module Model;
       )
     end
 
-    def self.inflate_xml(query_response)
-      return Lynr::Model::Vin.inflate(nil) if query_response.nil?
-      us_data = query_response.find('.//us_market_data/common_us_data').first
-      ext_colors = (us_data && us_data.find('.//exterior_colors//generic_color_name').map { |el| el.content }) || []
-      int_colors = (us_data && us_data.find('.//interior_colors//generic_color_name').map { |el| el.content }) || []
-      Lynr::Model::Vin.new(
-        us_data && us_data.find('.//transmission/@name').map { |n| n.value }.first,
-        us_data && us_data.find('.//fuel_type').map { |n| n.content }.first,
-        us_data && us_data.find('.//doors').map { |n| n.content }.first,
-        us_data && us_data.find('.//drive_type').map { |n| n.content }.first,
-        (ext_colors.length >= 1 && ext_colors.join(', ')) || ext_colors.first,
-        (int_colors.length >= 1 && int_colors.join(', ')) || int_colors.first,
-        query_response['identifier'],
-        query_response.to_s
-      )
-    end
-
   end
 
 end; end;
