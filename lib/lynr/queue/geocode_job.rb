ec601eb36959f6876da9f0df39b1ad057b617605
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-07 13:29:29 -0500

    Reorganize geocoding jobs

--- lib/lynr/queue/geocode_job.rb
+++ lib/lynr/queue/geocode_job.rb
@@ -1,64 +1,15 @@
 require 'rest-client'
 
 require './lib/lynr'
-require './lib/lynr/model/dealership'
-require './lib/lynr/queue/job'
+require './lib/lynr/queue'
+require './lib/lynr/queue/geocode_job/google'
+require './lib/lynr/queue/geocode_job/mapquest'
 
 module Lynr; class Queue;
 
-  class GeocodeJob < Job
+  module GeocodeJob
 
     UA = 'Lynr Address Geocoder - http://www.lynr.co'
-    URL = 'http://open.mapquestapi.com/geocoding/v1/address?key='
-
-    def initialize(dealership, appkey)
-      @dealership = dealership
-      @appkey = appkey
-      @address = @dealership.address
-    end
-
-    def perform
-      return failure("Address is not valid", :no_requeue) if !valid?
-      data = { 'location' => { 'street' => @address.line_one, 'postalCode' => @address.zip, 'country' => 'US' } }
-      headers = {
-        'Content-type' => 'application/json;charset="utf-8"',
-        'Accept' => 'application/json',
-        'Cache-Control' => 'no-cache',
-        'Pragma' => 'no-cache',
-        'Content-length' => data.to_json.length
-      }
-      url = ""
-      @response = RestClient.post url, data.to_json, headers
-      # Extract the geo data from @response
-      # If no geo data check response for error codes
-      # Set the geo data on @address
-      # Save the new address information to @dealership
-      # If ALL of those steps succeeded then Success
-      Success
-    rescue RestClient::Exception => rce
-      log.warn("Post to #{url} with #{data} failed... #{rce.to_s}")
-      failure("Post to #{url} failed. #{rce.to_s}")
-    end
-
-    def to_s
-      "#<#{self.class.name}:#{object_id} dealership=#{@dealership.id.to_s}, name=#{@dealership.name}>"
-    end
-
-    private
-
-    def geodata
-      json = JSON.parse(@response)
-      results = json['results'][0] || {}
-      locations = results['locations'] || []
-    end
-
-    def valid?
-      !@address.nil? &&
-          @address.line_one.is_a? String &&
-          @address.zip.is_a? String && 
-          @address.line_one.length > 3 &&
-          @address.zip.length >= 5
-    end
 
   end
 
---
+++ lib/lynr/queue/geocode_job/google.rb
@@ -0,0 +1,46 @@
+require 'geocoder'
+
+require './lib/lynr'
+require './lib/lynr/model/address'
+require './lib/lynr/model/dealership'
+require './lib/lynr/persist/dealership_dao'
+require './lib/lynr/queue/job'
+
+module Lynr; class Queue;
+
+  module GeocodeJob
+
+    class Google < Job
+
+      def initialize(dealership)
+        @dealership = dealership
+      end
+
+      def perform
+        description = "Geocode for #{@dealership.id} -- #{@dealership.address.line_one}, #{@dealersip.address.postcode} --"
+        results = Geocoder.search("#{@dealership.address.line_one}, #{@dealersip.address.postcode}")
+        if (results.length == 0)
+          return failure("#{description} returned no results", :no_requeue)
+        end
+        addresses = results.map do |result|
+          Lynr::Model::Address.new(
+            'line_one' => result.street_address,
+            'city' => result.city,
+            'state' => result.state_code,
+            'zip' => result.postal_code
+          )
+        end
+        dao = Lynr::Persist::DealershipDao.new
+        dao.save(@dealership.set(address: addresses.first) if addresses.first != @dealership.address
+        Success
+      rescue Geocoder::OverQueryLimitError
+        failure("#{description} after limit reached. Retrying later.")
+      rescue Geocoder::Error => ge
+        failure("#{description} errored. #{ge.class.name} -- #{ge.message}", :no_requeue)
+      end
+
+    end
+
+  end
+
+end; end;
