4d8c1e938ed2dc3e79ebc69c435c330b9379d56b
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-07 13:19:03 -0500

    Change how address data is accessed
    And how addresses are created

--- lib/lynr/queue/geocode_address_job.rb
+++ lib/lynr/queue/geocode_address_job.rb
@@ -2,6 +2,7 @@ require 'geocoder'
 
 require './lib/lynr'
 require './lib/lynr/model/address'
+require './lib/lynr/model/dealership'
 require './lib/lynr/queue/job'
 
 module Lynr; class Queue;
@@ -13,18 +14,17 @@ module Lynr; class Queue;
     end
 
     def perform
-      description = "Geocode for #{@dealership.id} -- #{@dealership.address}, #{@dealersip.postcode} --"
-      results = Geocoder.search("#{@dealership.address}, #{@dealersip.postcode}")
+      description = "Geocode for #{@dealership.id} -- #{@dealership.address.line_one}, #{@dealersip.address.postcode} --"
+      results = Geocoder.search("#{@dealership.address.line_one}, #{@dealersip.address.postcode}")
       if (results.length == 0)
         return failure("#{description} returned no results", :no_requeue)
       end
       addresses = results.map do |result|
         Lynr::Model::Address.new(
-          result.street_address,
-          '',
-          result.city,
-          result.state,
-          result.postal_code
+          'line_one' => result.street_address,
+          'city' => result.city,
+          'state' => result.state_code,
+          'zip' => result.postal_code
         )
       end
       # TODO: Store address information for dealership
