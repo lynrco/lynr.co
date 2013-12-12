0610cb3fa106f3de6ffb9250c2fc6752a1a02b76
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-12 16:25:44 -0500

    No longer a video frame to resize

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -1,13 +1,3 @@
 define(function(require) {
 
-  var frame = document.querySelector('.video-frame');
-  var ratio = frame.width / frame.height;
-  var evt = require('modules/domEvents');
-  evt.on(window, 'resize', resizeFrame);
-  resizeFrame();
-
-  function resizeFrame() {
-    frame.style.height = (frame.offsetWidth / ratio) + 'px';
-  }
-
 });
