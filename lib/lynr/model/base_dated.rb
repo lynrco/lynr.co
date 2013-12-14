6b71cbab76cf6e689d90fce4b0ec1b35a14cb4a8
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-14 03:00:43 -0500

    No functional change just semantics
    Don't call the variable vehicle, it isn't guarenteed to be that type and
    the name implies it.

--- lib/lynr/model/base_dated.rb
+++ lib/lynr/model/base_dated.rb
@@ -2,11 +2,11 @@ module Lynr; module Model;
 
   module BaseDated
 
-    def ==(vehicle)
-      return false unless vehicle.is_a? self.class
+    def ==(o)
+      return false unless o.is_a? self.class
       my_view = self.view.delete_if { |k,v| ['updated_at', 'created_at'].include?(k) }
-      ov_view = vehicle.view.delete_if { |k,v| ['updated_at', 'created_at'].include?(k) }
-      my_view == ov_view
+      o_view = o.view.delete_if { |k,v| ['updated_at', 'created_at'].include?(k) }
+      my_view == o_view
     end
 
   end
