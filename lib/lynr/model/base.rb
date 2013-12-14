ec1c44835469d5a1453783be55cd26c58f081498
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-14 03:03:17 -0500

    Add way for classes mixing this module to be explicit about equality rules
    Provided the thing we are comparing to is not a `::Hash`

--- lib/lynr/model/base.rb
+++ lib/lynr/model/base.rb
@@ -5,8 +5,12 @@ module Lynr; module Model;
   module Base
 
     def ==(o)
-      obj = (o.respond_to?(:view) && o.view) || o
-      view == obj
+      if (!o.is_a?(::Hash) && self.respond_to?(:equality_fields, include_priv=true))
+        equality_by_fields(o)
+      else
+        obj = (o.respond_to?(:view) && o.view) || o
+        view == obj
+      end
     end
 
     def to_json
@@ -15,6 +19,15 @@ module Lynr; module Model;
     
     def view
       { }
+    end
+
+    private
+
+    def equality_by_fields(o)
+      result = equality_fields.reduce(true) do |result, property|
+        result && o.respond_to?(property) && self.send(property) == o.send(property)
+      end
+      result
     end
 
   end
