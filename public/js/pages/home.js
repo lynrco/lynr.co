866c408d33bf9f45237f87484f0b31a11710d2b4
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-20 13:34:10 -0500

    Change how mixpanel tracks events

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -1,8 +1,19 @@
 define(function(require) {
 
   var mixpanel = require('mixpanel');
+  var evt = require('modules/domEvents');
 
-  mixpanel.track_pageview();
-  mixpanel.track_forms('form.signup', 'Signed up for Launch Notification');
+  function trackFormSubmit(e) {
+    mixpanel.track('signup.launch', { description: 'Signed up for Launch Notification' });
+  }
+
+  function setupHome() {
+    var form = document.querySelector('form.signup');
+    evt.on(form, 'submit', trackFormSubmit);
+
+    mixpanel.track('pageview', { title: document.title, url: window.location.pathname });
+  }
+
+  return setupHome;
 
 });
