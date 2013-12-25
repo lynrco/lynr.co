86a940760e990e7986d2542ad61a67e14036b541
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-24 22:00:00 -0500

    Add documentation for Sly::App

--- lib/sly.rb
+++ lib/sly.rb
@@ -11,6 +11,25 @@ module Sly
   DynaMap = Sly::URLMap.new
   Director = Sly::Router.new([])
 
+  # # `Sly::App`
+  #
+  # Entry point for running a `Sly` application as middleware. `Sly::App#call`
+  # is the Rack entry point for a web application. `Sly::App` uses `Innate::Optioned`
+  # to setup options for the Sly web application.
+  #
+  # If an error traces back to here it is likely coming out of `#call` and from
+  # a `Sly::Route` defined elsewhere and added to the `Sly::Router`.
+  #
+  # ## Options
+  #
+  # * `cascade`, whether or not to pass un-matched requests on to downstream apps.
+  #   Defaults to false but should be set to true if `Sly::App` is used as middleware.
+  # * `root`, directory the root resides in. Used as the basis for template path
+  #   generation. Defaults to directory of the script being executed.
+  # * `views`, path -- from root -- to the location of views. Defaults to 'views'.
+  # * `layouts`, path -- from root -- to the location of layouts. Defaults to 'layouts'.
+  # * `partials`, path -- from root -- to the location of partials. Defaults to 'partials'.
+  #
   class App
 
     include Innate::Optioned
