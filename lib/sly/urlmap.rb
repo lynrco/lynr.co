7cd0fc28fb505006aa8ba7b749d33bb348d15bc5
Bryan J Swift <bryan.j.swift@gmail.com>
2013-12-26 12:07:13 -0500

    Documentation for `Sly::URLMap`
    See #23

--- lib/sly/urlmap.rb
+++ lib/sly/urlmap.rb
@@ -3,8 +3,24 @@ require 'innate'
 
 module Sly
 
+  # # `Sly::URLMap`
+  #
+  # Modifies the base `Rack::URLMap` such that if another Rack app is added for a
+  # location which is already mapped both apps are put into a `Rack::Cascade` and
+  # the `Rack::Cascade` instances is provided for the location.
+  #
+  # See https://github.com/Ramaze/innate/blob/master/lib/innate/dynamap.rb
+  # which extends http://rack.rubyforge.org/doc/Rack/URLMap.html for more information.
+  #
   class URLMap < Innate::URLMap
 
+    # ## `Sly::URLMap#map(location, object)`
+    #
+    # If a Rack app already exists for `location` wrap the existing app in a new
+    # `Rack::Cascade` instance, add `object` to the new `Rack::Cascade` and send
+    # the `Cascade` to the parent class for `location`. If no app exists for `location`
+    # pass along both `location` and `object` to the parent class' implementation.
+    #
     def map(location, object)
       return unless location and object
       if (@originals.has_key? location)
