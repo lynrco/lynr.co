f744dfe040281f2245be10a0116b62340395d394
Bryan J Swift <bryan@lynr.co>
2013-12-28 09:24:08 -0500

    Documentation for `Lynr::View::Menu`
    See #23

--- lib/lynr/view/menu.rb
+++ lib/lynr/view/menu.rb
@@ -1,19 +1,32 @@
-require './lib/sly/view/erb_helpers'
-
 module Lynr; module View;
 
+  # # `Lynr::View::Menu`
+  #
+  # Simple view class to hold information about a menu so it can be rendered
+  # generically in a layout or template.
+  #
   class Menu
-
-    include Sly::View::ErbHelpers
 
     attr_reader :text, :href, :partial
 
+    # ## `Lynr::View::Menu.new(text, href, partial)`
+    #
+    # Create a new `Menu` instance holding information about text, href and partial.
+    # Text is what to display as the text for the menu icon. Href is the link to
+    # show the page with the menu content already shown. Partial is the menu template
+    # to render.
+    #
     def initialize(text, href, partial)
       @text = text
       @href = href
       @partial = partial
     end
 
+    # ## `Lynr::View::Menu#set_href(href)`
+    #
+    # Generate a new Menu instance with the same `text` and `partial` values but a
+    # new `href` value.
+    #
     def set_href(href)
       Menu.new(@text, href, @partial)
     end
