c6c860b6f94b87580e92c023dd678cd60972049d
Bryan J Swift <bryan@lynr.co>
2013-12-28 08:56:22 -0500

    Documentation for `Lynr::Worker`
    See #23

--- lib/lynr/worker.rb
+++ lib/lynr/worker.rb
@@ -15,17 +15,39 @@ require './lib/lynr/queue/stripe_update_job'
 
 module Lynr
 
+  # # `Lynr::Worker`
+  #
+  # Workhorse class for job queue processing. `Worker` creates a `JobQueue` and then
+  # subscribes to the channel waiting for message deliveries. Messges are processed
+  # one at a time by invoking the `Job`'s `#perform` method. `Worker` traps `QUIT`,
+  # `TERM`, and `INT` signals and invokes `Worker#stop` upon receipt of one of these
+  # signals to gracefully stop the `JobQueue` consumers and tell the `Process` to exit.
+  # The `#stop` method is also invoked when `Job#perform` or `JobQueue#subscribe` raise
+  # exceptions. Worker is a process aware wrapper around `Lynr::Queue::JobQueue`.
+  #
   class Worker
 
     include Lynr::Logging
 
     attr_reader :config
 
+    # ## `Lynr::Worker.new(queue_name)`
+    #
+    # Create a new `Worker` with `queue_name` as the name of the `JobQueue` used
+    # to process the `Worker`'s messages.
+    #
     def initialize(queue_name)
       @config = Lynr.config('app')
       @consumer = Lynr::Queue::JobQueue.new(queue_name, @config['amqp']['consumer'])
     end
 
+    # ## `Lynr::Worker#call`
+    #
+    # Set up the signal traps and message subscription. When backing `JobQueue`
+    # receives a message invoke its `perform` method and examine the result. If
+    # `perform` returns a successful result send an `ack` message to the queue,
+    # otherwise send a nack with the option to requeue based on the result.
+    #
     def call
       [:QUIT, :TERM, :INT].each do |sig|
         Signal.trap(sig) { stop }
@@ -41,9 +63,6 @@ module Lynr
           @consumer.nack(job.delivery_info.delivery_tag, result.requeue?)
         end
       end
-    rescue Bunny::ConnectionClosedError, Bunny::NetworkFailure => be
-      log.warn("#{queue_info} state=error message=`#{be.message}`")
-      stop
     rescue SystemExit => sysexit
       stop unless sysexit.success?
     rescue Exception => e
@@ -51,10 +70,19 @@ module Lynr
       stop
     end
 
+    # ## `Lynr::Worker#queue_info`
+    #
+    # Return a String with information about the process where the worker is running
+    # and the name of the consumer.
+    #
     def queue_info
       "pid=#{Process.pid} queue=#{@consumer.name}"
     end
 
+    # ## `Lynr::Worker#stop`
+    #
+    # Disconnect the consumer and tell the process to exit
+    #
     def stop
       log.info("#{queue_info} state=stopped")
       @consumer.disconnect
