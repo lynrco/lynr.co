336a2079a3881d426e35a8244ba2f6b10e2981d8
Bryan J Swift <bryan@lynr.co>
2013-12-28 08:17:05 -0500

    Documentation for `Rack::Middleware::Logger`
    See #23

--- lib/rack/middleware/logger.rb
+++ lib/rack/middleware/logger.rb
@@ -1,12 +1,28 @@
 module Rack; module Middleware;
 
+  # # `Rack::Middleware::Logger`
+  #
+  # Book keeping middleware to log a request happened and the status of the response.
+  #
   class Logger
 
+    # ## `Rack::Middleware::Logger.new(app, log)
+    #
+    # Middleware compatible constructor which accepts the downstream app to invoke.
+    # `Logger` constructor also accepts `log` which is instance where request/response
+    # information will be logged.
+    #
     def initialize(app, log)
       @app = app
       @log = log
     end
 
+    # ## `Rack::Middleware::Logger#call(env)`
+    #
+    # Standard Rack application method to invoke when running. Logs a message when
+    # a request comes in and another message after a responses is returned from the
+    # downstream application.
+    #
     def call(env)
       path = env['PATH_INFO']
       @log.info("type=request path=#{path}")
@@ -15,6 +31,10 @@ module Rack; module Middleware;
       [status, headers, self]
     end
 
+    # ## `Rack::Middleware::Logger#each(&block)`
+    #
+    # Standard Rack response iterator.
+    #
     def each(&block)
       @response.each(&block)
     end
