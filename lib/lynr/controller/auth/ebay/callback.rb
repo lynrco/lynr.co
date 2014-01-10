817aacc0ca1d746b35c70f4777731842b624420a
Bryan J Swift <bryan@lynr.co>
2014-01-10 17:38:49 -0500

    Store eBay authentication token on callback
    Closes #4 #5

--- lib/lynr/controller/auth/ebay/callback.rb
+++ lib/lynr/controller/auth/ebay/callback.rb
@@ -2,7 +2,10 @@ require 'yaml'
 
 require './lib/ebay'
 require './lib/lynr/cache'
+require './lib/lynr/model/accounts'
+require './lib/lynr/model/dealership'
 require './lib/lynr/model/ebay_account'
+require './lib/lynr/persist/dealership_dao'
 
 module Lynr::Controller
 
@@ -11,7 +14,7 @@ module Lynr::Controller
   # Resource/Controller class to handle the case where authentication with eBay
   # succeeds.
   #
-  class Ebay::Callback < Lynr::Controller::Base
+  class Ebay::Callback < Lynr::Controller::Admin
 
     get  '/auth/ebay/callback', :get
 
@@ -20,16 +23,31 @@ module Lynr::Controller
       @title = "Callback Success"
     end
 
+    # ## `Ebay::Callback#get(req)
+    #
+    # Process `req` to determine if dealership in request session has authorized
+    # us. If they have save the authorization token to the dealership instance and
+    # redirect the customer to the account details page.
+    #
     def get(req)
-      session_data = Lynr::Cache.mongo.get("#{req.session['dealer_id']}_ebay_session")
-      session = YAML.load(session_data)
+      # TODO: Make sure a session exists and is a dealership id
+      dealership = session_user(req)
+      session = get_session(req)
+      # TODO: Check session exists and is valid
       token = ::Ebay::Api.token(session)
       # TODO: Check `token` is valid
       @account = Lynr::Model::EbayAccount.new(
         'expires' => token.expires, 'token' => token.id, 'session' => session.id,
       )
-      Lynr::Cache.mongo.set("#{req.session['dealer_id']}_ebay_token", YAML.dump(token))
+      dealer_dao.save(dealership.set({ 'accounts' => Lynr::Model::Accounts.new([@account]) }))
       render 'auth/ebay/callback.erb', layout: 'default.erb'
+    end
+
+    private
+
+    def get_session(req)
+      session_data = Lynr::Cache.mongo.get("#{req.session['dealer_id']}_ebay_session")
+      YAML.load(session_data)
     end
 
   end
