5559e5bdbcdb5905ca20a2ad26a5d2588a971259
Bryan J Swift <bryan@lynr.co>
2014-01-10 13:16:46 -0500

    Specs and documentation for Accounts
    See #5 #23

--- lib/lynr/model/accounts.rb
+++ lib/lynr/model/accounts.rb
@@ -9,24 +9,42 @@ module Lynr::Model
   #
   class Accounts
 
+    # ## `Accounts.new(data)`
+    #
+    # Create `Accounts` with `data`. `data` is an `Array` of `Hash` instances representing
+    # external account types. Known account types are:
+    #
+    # * `Lynr::Model::EbayAccount`
+    #
     def initialize(data=[])
       @data = data || []
     end
 
+    # ## `Accounts#ebay`
+    #
+    # Get the `EbayAccount` from the `data` provided to the constructor. Provides an
+    # empty and expired `EbayAccount` if no `Hash` of type `EbayAccount::TYPE` was
+    # provided.
+    #
     def ebay
       return @ebay unless @ebay.nil?
-      ebay_data = @data.find { |a| a['type'] == EbayAcount::TYPE }
+      ebay_data = @data.find { |a| a['type'] == EbayAccount::TYPE }
       @ebay = EbayAccount.inflate(ebay_data)
     end
 
+    # ## `Accounts#view`
+    #
+    # Create a primitives only representation of this `Accounts` instance.
+    #
     def view
       view_data = []
       view_data.push(ebay.view) unless ebay.empty?
       view_data
     end
 
-    def self.inflate(data)
-      Accounts.new(data)
+    # `Accounts.inflate` behaves exactly like `Accounts.new` so alias it.
+    class << self
+      alias :inflate :new
     end
 
   end
