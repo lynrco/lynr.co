73d996969c5454cdfc04d3cf862d5f3691352dc9
Bryan Swift <bryan@lynr.co>
2014-01-12 21:24:48 -0500

    Super simple credit card number formatter
    See #9

--- public/js/pages/auth.js
+++ public/js/pages/auth.js
@@ -5,10 +5,21 @@ define(function(require) {
   };
 
   function initSignup() {
+    var card = require('modules/credit-card-numbers');
+    var cardNumber = document.querySelector('#card_number');
+    var evt = require('modules/dom-events');
     var setupStripeForm = require('modules/stripe')
     var styleCheckbox = require('modules/style-checkbox');
+
     setupStripeForm(document.querySelector('form.signup'));
     styleCheckbox(document.querySelector('#agree_terms'));
+
+    evt.on(cardNumber, 'keyup', function(e) {
+      // Only reformat if typed character is a digit or whitespace
+      if (!!/[\d ]/.test(String.fromCharCode(e.which))) {
+        cardNumber.value = card.format(cardNumber.value);
+      }
+    });
   }
 
   return api;
