1f20a3d047209678f93a96288f1294babab9f273
Bryan Swift <bryan@lynr.co>
2014-01-12 20:00:42 -0500

    Copy domEvents.js

---
+++ public/js/modules/dom-events.js
@@ -0,0 +1,48 @@
+/*!
+ * domEvents, a simple interface for interacting with DOM events
+ * (c) Bryan J Swift 2012
+ * https://github.com/bryanjswift/require-js-bootstrap
+ * license MIT
+ */
+define(function() {
+
+  // Functions taken from http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
+  function addEvent(obj, type, fn) {
+    if (obj.addEventListener) {
+      obj.addEventListener(type, fn, false);
+    } else if (obj.attachEvent) {
+      obj["e" + type + fn] = fn;
+      obj[type + fn] = function() { obj["e" + type + fn](window.event); };
+      obj.attachEvent("on" + type, obj[type + fn]);
+    }
+  }
+
+  function removeEvent(obj, type, fn) {
+    if (obj.removeEventListener) {
+      obj.removeEventListener(type, fn, false);
+    } else if (obj.detachEvent) {
+      obj.detachEvent("on" + type, obj[type + fn]);
+      obj[type + fn] = null;
+      obj["e" + type + fn] = null;
+    } else if (console && console.warn) {
+      console.warn('no way to remove event from', node, name);
+    }
+  }
+
+  function preventEvent(e) {
+    if (e && e.preventDefault) { e.preventDefault(); }
+    if (e) { e.returnValue = false; }
+  }
+
+  function stopEvent(e) {
+    if (e.stopPropagation) { e.stopPropagation(); }
+  }
+
+  return {
+    off: removeEvent,
+    on: addEvent,
+    prevent: preventEvent,
+    stop: stopEvent
+  };
+
+});
