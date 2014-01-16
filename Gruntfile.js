88fd03b6d79340a0ead2e88a8ec11ccdd8962296
Bryan Swift <bryan@lynr.co>
2014-01-15 23:21:25 -0500

    Sort of able to automate icon creation with grunticon

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,24 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    grunticon: {
+      icons: {
+        files: [
+          {
+            expand: true,
+            cwd: 'public',
+            src: ['svg/*.svg'],
+            dest: 'public'
+          }
+        ],
+        options: {
+          datasvgcss: '/css/icons.data.svg.css',
+          datapngcss: '/css/icons.data.png.css',
+          urlpngcss: '/css/icons.fallback.css',
+          pngfolder: '/img/icon'
+        }
+      }
+    },
     // /usr/local/share/npm/lib/node_modules/less/bin/lessc --source-map --source-map-url=/css/main.css.map --source-map-rootpath=https://lynr.co.local:9393/less public/less/main.less public/css/main.css
     less: {
       development: {
@@ -40,6 +58,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-grunticon');
   grunt.loadNpmTasks('grunt-svgmin');
 
   grunt.registerTask('default', ['less', 'watch']);
