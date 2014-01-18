5004013e1a720e5f1fb6af5cb24d80c89c2c9076
Bryan Swift <bryan@lynr.co>
2014-01-18 16:31:03 -0500

    Documentation for `Lynr::Controller::AdminAccount`
    See #23

--- lib/lynr/controller/admin/account.rb
+++ lib/lynr/controller/admin/account.rb
@@ -7,6 +7,10 @@ require './lib/lynr/queue/stripe_update_job'
 
 module Lynr; module Controller;
 
+  # # `Lynr::Controller::AdminAccount`
+  #
+  # Handles requests for the account information page.
+  #
   class AdminAccount < Lynr::Controller::Admin
 
     include Lynr::Model
@@ -14,11 +18,20 @@ module Lynr; module Controller;
     get  '/admin/:slug/account', :get_account
     post '/admin/:slug/account', :post_account
 
+    # ## `AdminAccount#before_each(req)`
+    #
+    # Return an unauthorized error message if the dealership in `req.session` doesn't
+    # isn't the dealership being requested.
+    #
     def before_each(req)
       return unauthorized unless authorized?(req)
       @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
     end
 
+    # ## `AdminAccount#get_account(req)`
+    #
+    # Handle GET request for the account information page.
+    #
     def get_account(req)
       @subsection = 'account'
       @title = "Account Information"
@@ -29,6 +42,12 @@ module Lynr; module Controller;
       render 'admin/account.erb'
     end
 
+    # ## `AdminAccount#post_account(req)`
+    #
+    # Handle POST request for the account information page by inflating objects and
+    # updating data. Schedules background jobs to update information on payment
+    # gateway if necessary.
+    #
     def post_account(req)
       @posted = req.POST
       @errors = validate_account_info
@@ -44,22 +63,45 @@ module Lynr; module Controller;
 
     protected
 
-    # ## Logic Helpers
-
+    # ## `AdminAccount#connect_message(req)`
+    #
+    # *Protected* Entry point for communicating with external account connection
+    # paths. The account information page is the starting point for OAuth flows so
+    # it is the logical place to display error or success messages related to those
+    # external authentication flows. Reads parameters from `req` and returns a String
+    # to be used in `@msg` for display on the page.
+    #
     def connect_message(req)
       if !req.params[Lynr::Controller::Ebay::Helpers::PARAM].nil?
         Lynr::Controller::Ebay::Helpers.connect_message(req)
       end
     end
 
+    # ## `AdminAccount#email_changed?`
+    #
+    # *Protected* Check if the `email` on the current `@dealership` is different
+    # from the `email` in the request. Used to determine if external updates need
+    # to be made.
+    #
     def email_changed?
       @dealership.identity.email != posted['email']
     end
 
+    # ## `AdminAccount#name_changed?`
+    #
+    # *Protected* Check if the `name` on the current `@dealership` is different
+    # from the `name` in the request. Used to determine if external updates need
+    # to be made.
+    #
     def name_changed?
       @dealership.name != posted['name']
     end
 
+    # ## `AdminAccount#notify_by_email`
+    #
+    # *Protected* Add a background job to notify the old email address that the
+    # email address has been changed.
+    #
     def notify_by_email
       Lynr.producer('email').publish(Lynr::Queue::EmailJob.new('email_updated', {
         to: @dealership.identity.email,
@@ -67,7 +109,12 @@ module Lynr; module Controller;
       }))
     end
 
-    # ## Translate to image model
+    # ## `AdminAccount#translate_image`
+    #
+    # *Protected* Transform JSON data in a POST request into a
+    # `Lynr::Model::SizedImage` or use the existing `@dealership` if no image
+    # data exists in the POST.
+    #
     def translate_image
       if !posted['image'].nil? && !posted['image'].empty?
         json = JSON.parse(posted['image'])
@@ -77,13 +124,21 @@ module Lynr; module Controller;
       end
     end
 
-    # ## Stripe Helper
+    # ## `AdminAccount#update_stripe(dealership)`
+    #
+    # *Protected* Add a background job to notify the Stripe payment gateway
+    # customer information has changed for `dealership`.
+    #
     def update_stripe(dealership)
       Lynr.producer('stripe').publish(Lynr::Queue::StripeUpdateJob.new(dealership))
     end
 
-    # ## Data Validation
-
+    # ## `AdminAccount#validate_account_info`
+    #
+    # *Protected* Check `posted` data in the request is valid. Returns a `Hash`
+    # with error information. `Hash` is empty if no errors, otherwise key value
+    # pairs are of the form `field name => error message`.
+    #
     def validate_account_info
       errors = validate_required(posted, ['email'])
       email = posted['email']
