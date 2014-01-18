b26715007bc2cdcec7de2f30c9ce5cc4ea71edc3
Bryan Swift <bryan@lynr.co>
2014-01-18 17:01:03 -0500

    Documentation for `Lynr::Model::Mpg`
    See #23

--- lib/lynr/model/mpg.rb
+++ lib/lynr/model/mpg.rb
@@ -4,12 +4,24 @@ require './lib/lynr/model/base'
 
 module Lynr; module Model;
 
+  # # `Lynr::Model::Mpg`
+  #
+  # Representation for miles per gallon information of a vehicle in different
+  # conditions.
+  #
   class Mpg
 
     include Base
 
     attr_reader :city, :highway
 
+    # ## `Mpg.new(data)`
+    #
+    # Extract values from `data` to store.
+    #
+    # * `city` is the MPG rating for a vehicle in city condtions
+    # * `highway` is the MPG rating for a vehicle in highway conditions
+    #
     def initialize(data={})
       @city = data['city'] || 0.0
       @highway = data['highway'] || 0.0
@@ -19,6 +31,11 @@ module Lynr; module Model;
       { 'city' => @city, 'highway' => @highway }
     end
 
+    # ## `Mpg.inflate`
+    #
+    # Transform a DB `record` into something `Mpg.new` will understand then
+    # return `Mpg.new`.
+    #
     def self.inflate(record)
       data = record || {}
       Lynr::Model::Mpg.new(data)
