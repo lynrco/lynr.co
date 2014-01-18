1a809cc2e890bfdb71e41f63e729c801c3a82951
Bryan Swift <bryan@lynr.co>
2014-01-18 16:54:29 -0500

    Documentation for `Lynr::Controller::AdminVin`
    See #23

--- lib/lynr/controller/admin/vin.rb
+++ lib/lynr/controller/admin/vin.rb
@@ -1,14 +1,29 @@
+require 'libxml'
+
 require './lib/lynr/controller/admin'
 require './lib/lynr/converter/data_one'
 
 module Lynr; module Controller;
 
+  # # `Lynr::Controller::AdminVin`
+  #
+  # Controller to process requests for the vin search resource.
+  #
   class AdminVin < Lynr::Controller::Admin
 
     include Lynr::Converter::DataOne
 
     post '/admin/:slug/vin/search', :search
 
+    # ## `AdminVin#search(req)`
+    #
+    # Process POST requests for VIN search by fetching data from the DataOne
+    # back end. If a vehicle is found with the provided VIN then create a new
+    # `Lynr::Model::Vehicle` based on the provided data, save it and redirect
+    # to the edit page for the newly created vehicle. If no data is found for
+    # the provided VIN then display an error on the add vehicle page notifying
+    # the customer.
+    #
     def search(req)
       return unauthorized unless authorized?(req)
       @dealership = dealer_dao.get(BSON::ObjectId.from_string(req['slug']))
@@ -30,6 +45,11 @@ module Lynr; module Controller;
       end
     end
 
+    # ## `AdminVin#fetch(vin)`
+    #
+    # Retrieve vehicle data from the the DataOne back end and return a
+    # `LibXML::XML::Node` with the `<query_response />` element from the data.
+    #
     def fetch(vin)
       if File.exists?("spec/data/#{vin}.xml")
         doc = LibXML::XML::Document.file("spec/data/#{vin}.xml")
