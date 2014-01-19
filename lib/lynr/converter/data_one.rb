3cb5cffadcd026560a14a123a70876fe9f7446cd
Bryan J Swift <bryan@lynr.co>
2014-01-19 01:53:48 -0500

    Use new `#content` and `#value` methods in LibXmlHelper

--- lib/lynr/converter/data_one.rb
+++ lib/lynr/converter/data_one.rb
@@ -26,7 +26,7 @@ module Lynr; module Converter;
       return Lynr::Model::Vehicle.new if query_response.nil?
       us_data = query_response.find('.//us_market_data/common_us_data').first
       Lynr::Model::Vehicle.new({
-        'price' => contents(us_data, './pricing/msrp').first,
+        'price' => content(us_data, './pricing/msrp'),
         'mpg' => xml_to_mpg(query_response),
         'vin' => xml_to_vin(query_response)
       })
@@ -42,8 +42,8 @@ module Lynr; module Converter;
       return Lynr::Model::Mpg.new if query_response.nil?
       us_data = query_response.find('.//us_market_data/common_us_data').first
       Lynr::Model::Mpg.new({
-        'city'    => contents(us_data, './/epa_fuel_efficiency/epa_mpg_record/city').first,
-        'highway' => contents(us_data, './/epa_fuel_efficiency/epa_mpg_record/highway').first
+        'city'    => content(us_data, './/epa_fuel_efficiency/epa_mpg_record/city'),
+        'highway' => content(us_data, './/epa_fuel_efficiency/epa_mpg_record/highway')
       })
     end
 
@@ -60,17 +60,17 @@ module Lynr; module Converter;
       ext_colors = contents(us_data, './/exterior_colors//generic_color_name')
       int_colors = contents(us_data, './/interior_colors//generic_color_name')
       model = [
-        contents(basic_data, './model').first,
-        contents(basic_data, './trim').first
+        content(basic_data, './model'),
+        content(basic_data, './trim')
       ].join(' ').strip
       Lynr::Model::Vin.new(
-        'year' => contents(basic_data, './year').first,
-        'make' => contents(basic_data, './make').first,
+        'year' => content(basic_data, './year'),
+        'make' => content(basic_data, './make'),
         'model' => (model unless model.nil? || model.empty?),
-        'transmission' => values(us_data, './/transmission/@name').first,
-        'fuel' => contents(us_data, './/fuel_type').first,
-        'doors' => contents(us_data, './/doors').first,
-        'drivetrain' => contents(us_data, './/drive_type').first,
+        'transmission' => value(us_data, './/transmission/@name'),
+        'fuel' => content(us_data, './/fuel_type'),
+        'doors' => content(us_data, './/doors'),
+        'drivetrain' => content(us_data, './/drive_type'),
         'ext_color' => (ext_colors.length >= 1 && ext_colors.join(', ')) || ext_colors.first,
         'int_color' => (int_colors.length >= 1 && int_colors.join(', ')) || int_colors.first,
         'number' => query_response['identifier'],
