1d299408c81fd54933ab061e950ea823f979ef0c
Bryan J Swift <bryan@lynr.co>
2014-01-19 02:27:33 -0500

    Reduce the logical complexity of `Vin`
    Might hurt the readability of it though.

--- lib/lynr/model/vin.rb
+++ lib/lynr/model/vin.rb
@@ -8,40 +8,31 @@ module Lynr; module Model;
 
     include Base
 
-    attr_reader :number, :raw
-    attr_reader :doors, :drivetrain, :ext_color, :fuel, :int_color, :make, :model,\
-                :transmission, :year
+    ATTRS = [ :number, :raw,
+              :doors, :drivetrain, :ext_color, :fuel, :int_color, :make, :model,
+              :transmission, :year
+            ]
 
     def initialize(data={})
-      @doors = data.fetch('doors', default=nil)
-      @drivetrain = data.fetch('drivetrain', default=nil)
-      @ext_color = data.fetch('ext_color', default=nil)
-      @fuel = data.fetch('fuel', default=nil)
-      @int_color = data.fetch('int_color', default=nil)
-      @make = data.fetch('make', default=nil)
-      @model = data.fetch('model', default=nil)
-      @transmission = data.fetch('transmission', default=nil)
-      @year = data.fetch('year', default=nil)
+      properties = data.dup
+      properties.delete_if { |k, v| v.is_a?(String) && v.empty? }
+      @data = properties
+    end
 
-      @data = data
-      @number = data.fetch('number', default=nil)
-      @raw = data.fetch('raw', default=nil)
+    def method_missing(name, *args, &block)
+      if args.size == 0 && block.nil? && (ATTRS.include?(name.to_sym))
+        @data.fetch(name.to_s, default=nil)
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(sym, include_private)
+      super || ATTRS.include?(sym)
     end
 
     def view
-      {
-        'doors' => @doors,
-        'drivetrain' => @drivetrain,
-        'ext_color' => @ext_color,
-        'fuel' => @fuel,
-        'int_color' => @int_color,
-        'make' => @make,
-        'model' => @model,
-        'transmission' => @transmission,
-        'year' => @year,
-        'number' => @number,
-        'raw' => @raw
-      }
+      Hash[ ATTRS.map { |key| [key.to_s, @data.fetch(key.to_s, default=nil)] } ]
     end
 
     def self.inflate(record)
