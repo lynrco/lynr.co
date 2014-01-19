6b9a9c28aae1ab0eb8fe547f522b96c565800068
Bryan Swift <bryan@lynr.co>
2014-01-19 14:47:57 -0500

    DRY conversion form upload results to image object
    Results from uploading account and vehicle images are translated the
    same way so represent that in the code.
    
    Closes #76

--- public/js/modules/transloadit-form.js
+++ public/js/modules/transloadit-form.js
@@ -34,48 +34,7 @@ define(['jquery', 'jquery.transloadit', 'modules/spinner'], function($, jtl, spi
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
   }
 
-  function transloaditOpts(page) {
-    var specific;
-    switch (page) {
-      case 'vehicle':
-        specific = {
-          onSuccess: uploadPhotosSuccess,
-          fields: 'input[name=idx], input[name=dealership_id], input[name=vehicle_id]'
-        };
-        break;
-      case 'account':
-        specific = { onSuccess: uploadAccountSuccess };
-        break;
-      default:
-        specific = {};
-        break;
-    }
-    return $.extend({}, baseTransloaditOpts, specific);
-  }
-
-  function uploadAccountSuccess(assembly) {
-    uploadSuccessStopSpinner(assembly);
-    var results = assembly.results;
-    var input = $('input[name=image]');
-    var original = results[':original'][0];
-    var full = results.optimized_full[0];
-    var thumb = results.optimized_thumb[0];
-    var image = {
-      url: full.url,
-      src: full.url,
-      width: full.meta.width,
-      height: full.meta.height
-    };
-    input.val(JSON.stringify(image));
-    $('img.f-image-preview').attr(image).removeClass('f-image-preview-empty icon-add-photo');
-  }
-
-  function uploadPhotosSuccess(assembly) {
-    uploadSuccessStopSpinner(assembly);
-    var results = assembly.results;
-    var fields = assembly.fields;
-    var form = $('#photo-' + fields.idx);
-    var input = $('input[name=images]');
+  function imageFromResults(results) {
     var original = results[':original'][0];
     var full = results.optimized_full[0];
     var thumb = results.optimized_thumb[0];
@@ -99,6 +58,44 @@ define(['jquery', 'jquery.transloadit', 'modules/spinner'], function($, jtl, spi
         height: thumb.meta.height
       }
     };
+    return image;
+  }
+
+  function transloaditOpts(page) {
+    var specific;
+    switch (page) {
+      case 'vehicle':
+        specific = {
+          onSuccess: uploadPhotosSuccess,
+          fields: 'input[name=idx], input[name=dealership_id], input[name=vehicle_id]'
+        };
+        break;
+      case 'account':
+        specific = { onSuccess: uploadAccountSuccess };
+        break;
+      default:
+        specific = {};
+        break;
+    }
+    return $.extend({}, baseTransloaditOpts, specific);
+  }
+
+  function uploadAccountSuccess(assembly) {
+    uploadSuccessStopSpinner(assembly);
+    var results = assembly.results;
+    var input = $('input[name=image]');
+    var image = imageFromResults(results);
+    input.val(JSON.stringify(image));
+    $('img.f-image-preview').attr(image).removeClass('f-image-preview-empty icon-add-photo');
+  }
+
+  function uploadPhotosSuccess(assembly) {
+    uploadSuccessStopSpinner(assembly);
+    var results = assembly.results;
+    var fields = assembly.fields;
+    var form = $('#photo-' + fields.idx);
+    var input = $('input[name=images]');
+    var image = imageFromResults(results);
     var images = JSON.parse(input.val());
     images[fields.idx] = image;
     input.val(JSON.stringify(images));
