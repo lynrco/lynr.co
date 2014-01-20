74cc11b33be62d2cf8a34dcd8a01f155fd383915
Bryan Swift <bryan@lynr.co>
2014-01-20 01:42:17 -0500

    Pull lessFiles to compile out of less task config

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function(grunt) {
 
   var requirejs = require('requirejs');
 
+  var lessFiles = {
+    "public/css/main.css": "public/less/main.less",
+    "public/css/marketing.css": "public/less/marketing.less",
+    "public/css/icons.data.png.css": "public/less/icons.data.png.less",
+    "public/css/icons.data.svg.css": "public/less/icons.data.svg.less"
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     // /usr/local/share/npm/lib/node_modules/less/bin/lessc --source-map --source-map-url=/css/main.css.map --source-map-rootpath=https://lynr.co.local:9393/less public/less/main.less public/css/main.css
@@ -11,10 +18,7 @@ module.exports = function(grunt) {
           paths: ['public/less'],
           sourceMap: true
         },
-        files: {
-          "public/css/main.css": "public/less/main.less",
-          "public/css/marketing.css": "public/less/marketing.less"
-        }
+        files: lessFiles
       }
     },
     svgmin: {
