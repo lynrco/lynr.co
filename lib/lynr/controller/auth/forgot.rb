4a0786b4fd91dfbfea4642f88f2a7758fe62fef9
Bryan Swift <bryan@lynr.co>
2014-02-05 23:15:04 -0800

    Add documentation for `Lynr::Controller::Auth::Forgot`

--- lib/lynr/controller/auth/forgot.rb
+++ lib/lynr/controller/auth/forgot.rb
@@ -3,18 +3,33 @@ require './lib/lynr/persist/dao'
 
 module Lynr::Controller
 
+  # # `Lyrn::Controller::Auth::Forgot`
+  #
+  # Controller for the forgot password actions.
+  #
   class Auth::Forgot < Lynr::Controller::Auth
 
-    get  '/signin/forgot',  :get_forgot
-    post '/signin/forgot',  :post_forgot
+    get  '/signin/forgot',  :get
+    post '/signin/forgot',  :post
 
-    def get_forgot(req)
+    # ## `Auth::Forgot#get(req)`
+    #
+    # Process GET `req` into the rendered HTML for showing the forgot
+    # password page.
+    #
+    def get(req)
       @subsection = "forgot"
       @title = "Forgot Password"
       render 'auth/forgot.erb'
     end
 
-    def post_forgot(req)
+    # ## `Auth::Forgot#get(req)`
+    #
+    # Process POST `req` into the rendered HTML for showing the forgot
+    # password page in addition to performing the processing of creating a token
+    # for simple authentication and sending an email about the token.
+    #
+    def post(req)
       @subsection = "forgot"
       @title = "Forgot Password"
       @errors = validate_forgot(@posted)
@@ -29,6 +44,11 @@ module Lynr::Controller
 
     protected
 
+    # ## `Auth::Forgot#notifiy_by_email(dealership, token, req)`
+    #
+    # Send a notification email to the `Identity` associated with `dealership`
+    # that password assistance has been requested.
+    #
     def notify_by_email(dealership, token, req)
       Lynr.producer('email').publish(Lynr::Queue::EmailJob.new('auth/forgot', {
         to: dealership.identity.email,
@@ -38,6 +58,11 @@ module Lynr::Controller
       }))
     end
 
+    # ## `Auth::Forgot#validate_forgot(posted)`
+    #
+    # Check the data in `posted` for validation errors, specifically looking
+    # to make sure email is well formed and is tied to an existing user account.
+    #
     def validate_forgot(posted)
       errors = validate_required(posted, ['email'])
       email = posted['email']
