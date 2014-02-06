6470ee516b00778f5fe3575f5be1255f20c28156
Bryan Swift <bryan@lynr.co>
2014-02-05 22:39:13 -0800

    Handle POST request for forgot password
    Closes #101

--- lib/lynr/controller/auth/forgot.rb
+++ lib/lynr/controller/auth/forgot.rb
@@ -6,11 +6,41 @@ module Lynr::Controller
   class Auth::Forgot < Lynr::Controller::Auth
 
     get  '/signin/forgot',  :get_forgot
+    post '/signin/forgot',  :post_forgot
 
     def get_forgot(req)
       @subsection = "forgot"
       @title = "Forgot Password"
       render 'auth/forgot.erb'
+    end
+
+    def post_forgot(req)
+      @subsection = "forgot"
+      @title = "Forgot Password"
+      @errors = validate_forgot(@posted)
+      return render 'auth/forgot.erb' if has_errors?
+      dealership = dealer_dao.get_by_email(@posted['email'])
+      dao = Lynr::Persist::Dao.new
+      token = dao.create(Lynr::Model::Token.new('dealership' => dealership))
+      # Produce `EmailJob` to notify customer
+      @msg = "Reset notification sent to #{@posted['email']}"
+      render 'auth/forgot.erb'
+    end
+
+    protected
+
+    def validate_forgot(posted)
+      errors = validate_required(posted, ['email'])
+      email = posted['email']
+
+      errors['email'] ||=
+        if !is_valid_email?(email)
+          "#{email} doesn't look valid."
+        elsif !dealer_dao.account_exists?(email)
+          "No account for #{email}."
+        end
+
+      errors.delete_if { |k,v| v.nil? }
     end
 
   end
