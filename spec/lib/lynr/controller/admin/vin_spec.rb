5e9e722d737fcd6d98ead9afe6d66647a423d0b4
Bryan Swift <bryan@lynr.co>
2014-02-13 14:40:31 -0800

    Add specs for structure of dataone query XML
    Specs for this structure are important because it directly impacts the
    set of data which will be returned by the DataOne API.
    
    See #15

--- spec/lib/lynr/controller/admin/vin_spec.rb
+++ spec/lib/lynr/controller/admin/vin_spec.rb
@@ -6,14 +6,107 @@ require './lib/lynr/controller/admin/vin'
 describe Lynr::Controller::AdminVin do
 
   let(:controller) { Lynr::Controller::AdminVin.new }
+  let(:query) { controller.dataone_xml_query('1HGEJ6229XL063838') }
+  let(:doc) { LibXML::XML::Document.string(query) }
 
   describe "#dataone_xml_query" do
 
     it "contains a <query_request> element" do
-      query_request = controller.dataone_xml_query('1HGEJ6229XL063838')
-      doc = LibXML::XML::Document.string(query_request)
-      els = doc.find('.//query_request')
-      expect(els.length).to be > 0
+      expect(doc.find('.//query_request').length).to be > 0
+    end
+
+    context "<query_request>" do
+
+      let(:query_request) { doc.find('.//query_request').first }
+
+      it "has identifier equal to vin" do
+        expect(controller.value(query_request, './@identifier')).to eq('1HGEJ6229XL063838')
+      end
+
+      it "has a <vin /> with content equal to vin number requested" do
+        expect(controller.content(query_request, './vin')).to eq('1HGEJ6229XL063838')
+      end
+
+    end
+
+    it "contains a <decoder_settings> element" do
+      expect(doc.find('.//decoder_settings').length).to be > 0
+    end
+
+    context "<decoder_settings>" do
+
+      let(:decoder_settings) { doc.find('.//decoder_settings').first }
+
+      it "has a <version /> of 7.0.1" do
+        expect(controller.content(decoder_settings, './version')).to eq('7.0.1')
+      end
+
+      it "has <style_data_packs /> information" do
+        expect(decoder_settings.find('./style_data_packs').length).to be > 0
+      end
+
+      it "has <common_data_packs /> information" do
+        expect(decoder_settings.find('./common_data_packs').length).to be > 0
+      end
+
+      it "has <styles> on" do
+        expect(controller.content(decoder_settings, './styles')).to eq('on')
+      end
+
+      it "has <common_data> on" do
+        expect(controller.content(decoder_settings, './common_data')).to eq('on')
+      end
+
+      context "<style_data_packs>" do
+
+        let(:style_data_packs) { decoder_settings.find('./style_data_packs').first }
+
+        it "has <basic_data> on" do
+          expect(controller.content(style_data_packs, './basic_data')).to eq('on')
+        end
+
+        it "has <specifications> on" do
+          expect(controller.content(style_data_packs, './specifications')).to eq('on')
+        end
+
+        it "has <engines> on" do
+          expect(controller.content(style_data_packs, './engines')).to eq('on')
+        end
+
+        it "has <transmissions> on" do
+          expect(controller.content(style_data_packs, './transmissions')).to eq('on')
+        end
+
+        it "has <installed_equipment> on" do
+          expect(controller.content(style_data_packs, './installed_equipment')).to eq('on')
+        end
+
+        it "has <safety_equipment> on" do
+          expect(controller.content(style_data_packs, './safety_equipment')).to eq('on')
+        end
+
+        it "has <optional_equipment> on" do
+          expect(controller.content(style_data_packs, './optional_equipment')).to eq('on')
+        end
+
+        it "has <generic_optional_equipment> on" do
+          expect(controller.content(style_data_packs, './generic_optional_equipment')).to eq('on')
+        end
+
+        it "has <colors> on" do
+          expect(controller.content(style_data_packs, './colors')).to eq('on')
+        end
+
+        it "has <fuel_efficiency> on" do
+          expect(controller.content(style_data_packs, './fuel_efficiency')).to eq('on')
+        end
+
+        it "has <pricing> on" do
+          expect(controller.content(style_data_packs, './pricing')).to eq('on')
+        end
+
+      end
+
     end
 
   end
