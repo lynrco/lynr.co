336a8307eed5e926ab5f6146105a086ce4c9f883
Bryan Swift <bryan@lynr.co>
2014-02-21 17:02:15 -0800

    Add documentation for `Lynr::Model::Slug`
    See #23

--- lib/lynr/model/slug.rb
+++ lib/lynr/model/slug.rb
@@ -1,9 +1,18 @@
-require 'bson'
-
 module Lynr; module Model;
 
+  # # `Lynr::Model::Slug`
+  #
+  # Encapsulates the logic necessary to turn an arbitrary string into a URI
+  # safe String, where URI safe means alphanumeric and hyphens.
+  #
   class Slug < String
 
+    # ## `Slug.new(name, default)`
+    #
+    # Creates a new String subclass by calling `Slug.slugify` on `name`
+    # provided it is not nil or emtpty. If `name` is nil or empty use
+    # `default` instead.
+    #
     def initialize(name, default)
       if name.nil? or name.empty?
         super(default.to_s)
@@ -12,6 +21,10 @@ module Lynr; module Model;
       end
     end
 
+    # ## `Slug.slugify(str)`
+    #
+    # Perform the act of turning an existing String into a URI safe string.
+    #
     def self.slugify(str)
       str.strip.downcase.gsub /\W+/, '-'
     end
