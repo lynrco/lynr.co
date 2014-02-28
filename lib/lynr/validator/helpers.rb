3b8ea4b844a02797f6969145ecfbf4b04ef7610a
Bryan Swift <bryan@lynr.co>
2014-02-28 13:14:14 -0800

    Add `#error_for_slug` method to get error message
    Consolidate the logic for how slug errors are reported into
    `Lynr::Validator::Helpers#error_for_slug`. Add specs for the new
    `#error_for_slug` method.
    
    See #61

--- lib/lynr/validator/helpers.rb
+++ lib/lynr/validator/helpers.rb
@@ -8,6 +8,31 @@ module Lynr; module Validator;
   #
   module Helpers
 
+    # ## `Helpers#error_for_slug(dao, slug)`
+    #
+    # Check the validity with `#is_valid_slug?` and existence of a slug with
+    # `dao` and return the appropriate error message if slug is not valid or
+    # exists in the database.
+    #
+    def error_for_slug(dao, slug)
+      if !is_valid_slug?(slug)
+        "Dealership handle may contain only lowercase letters, numbers and hyphens."
+      elsif (dao.slug_exists?(slug))
+        "Dealership handle, <em>#{slug}</em>, is in use by someone else."
+      else
+        nil
+      end
+    end
+
+    # ## `Helpers#is_valid_slug?(slug)`
+    #
+    # Check to see if `slug` should be permitted. Slugs must contain only
+    # lowercase letters and hyphens.
+    #
+    def is_valid_slug?(slug)
+      BSON::ObjectId.legal?(slug) || !(%r(^[a-z0-9-]+$) =~ slug).nil?
+    end
+
     # ## `Lynr::Validator::Helpers#validate_required(posted, fields)`
     #
     # Check that `posted` contains a non-nil, non-empty value for each key in `fields`.
@@ -21,15 +46,6 @@ module Lynr; module Validator;
       end
 
       errors
-    end
-
-    # ## `Helpers#is_valid_slug?(slug)`
-    #
-    # Check to see if `slug` should be permitted. Slugs must contain only
-    # lowercase letters and hyphens.
-    #
-    def is_valid_slug?(slug)
-      BSON::ObjectId.legal?(slug) || !(%r(^[a-z-]+$) =~ slug).nil?
     end
 
   end
--- spec/lib/lynr/validator/helpers_spec.rb
+++ spec/lib/lynr/validator/helpers_spec.rb
@@ -1,4 +1,8 @@
 require 'rspec/autorun'
+require './spec/spec_helper'
+
+require './lib/lynr/model/dealership'
+require './lib/lynr/persist/dealership_dao'
 require './lib/lynr/validator'
 
 describe Lynr::Validator::Helpers do
@@ -7,7 +11,10 @@ describe Lynr::Validator::Helpers do
     include Lynr::Validator::Helpers
   end
 
+  let(:dealer) { Lynr::Model::Dealership.new({ 'slug' => 'carmax-san-diego', 'identity' => identity }) }
+  let(:dao) { Lynr::Persist::DealershipDao.new }
   let(:helpers) { Dummy.new }
+  let(:identity) { Lynr::Model::Identity.new('bryan@lynr.co', 'this is a fake password') }
   let(:post) {
     {
       'hi' => '',
@@ -72,6 +79,45 @@ describe Lynr::Validator::Helpers do
 
     it "returns false if contains uppercase letters" do
       expect(helpers.is_valid_slug?('Say-hi')).to be_false
+    end
+
+    it "returns true if contains letters and numbers" do
+      expect(helpers.is_valid_slug?('say7hi')).to be_true
+    end
+
+    it "returns true if contains numbers and hyphens" do
+      expect(helpers.is_valid_slug?('7-11')).to be_true
+    end
+
+    it "returns true if contains letters, numbers and hyphens" do
+      expect(helpers.is_valid_slug?('welcome-7-11')).to be_true
+    end
+
+  end
+
+  context "with mongo connection", :if => (MongoHelpers.connected?) do
+
+    before(:each) do
+      MongoHelpers.empty! if MongoHelpers.connected?
+    end
+
+    describe "#error_for_slug" do
+
+      it "gives an error if slug is invalid" do
+        expect(helpers.error_for_slug(dao, "Say-hi")).to eq(\
+          "Dealership handle may contain only lowercase letters, numbers and hyphens.")
+      end
+
+      it "gives an error if slug is in use" do
+        dao.save(dealer)
+        expect(helpers.error_for_slug(dao, "carmax-san-diego")).to eq(\
+          "Dealership handle, <em>carmax-san-diego</em>, is in use by someone else.")
+      end
+
+      it "gives nil if slug valid and not in use" do
+        expect(helpers.error_for_slug(dao, "say-hi")).to be_nil
+      end
+
     end
 
   end
