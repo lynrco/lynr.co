3fbc64ceebe98d53aa0ede3e1535b36a8f3fd7aa
Bryan Swift <bryan@lynr.co>
2014-03-14 11:08:24 -0700

    Extract some of the logic out of `xml_to_vin`
    And use the libxml `find_first` method instead of `find` then calling
    `first` on the resulting set of nodes.

--- lib/lynr/converter/data_one.rb
+++ lib/lynr/converter/data_one.rb
@@ -24,7 +24,7 @@ module Lynr; module Converter;
     #
     def xml_to_vehicle(query_response)
       return Lynr::Model::Vehicle.new if query_response.nil?
-      us_data = query_response.find('.//us_market_data/common_us_data').first
+      us_data = query_response.find_first('.//us_market_data/common_us_data')
       Lynr::Model::Vehicle.new({
         'price' => content(us_data, './pricing/msrp'),
         'mpg' => xml_to_mpg(query_response),
@@ -40,7 +40,7 @@ module Lynr; module Converter;
     #
     def xml_to_mpg(query_response)
       return Lynr::Model::Mpg.new if query_response.nil?
-      us_data = query_response.find('.//us_market_data/common_us_data').first
+      us_data = query_response.find_first('.//us_market_data/common_us_data')
       Lynr::Model::Mpg.new({
         'city'    => content(us_data, './/epa_fuel_efficiency/epa_mpg_record/city'),
         'highway' => content(us_data, './/epa_fuel_efficiency/epa_mpg_record/highway')
@@ -55,27 +55,41 @@ module Lynr; module Converter;
     #
     def xml_to_vin(query_response)
       return Lynr::Model::Vin.inflate(nil) if query_response.nil?
-      us_data = query_response.find('.//us_market_data/common_us_data').first
-      basic_data = us_data.find('./basic_data').first unless us_data.nil?
-      ext_colors = contents(us_data, './/exterior_colors//generic_color_name')
-      int_colors = contents(us_data, './/interior_colors//generic_color_name')
-      model = [
-        content(basic_data, './model'),
-        content(basic_data, './trim')
-      ].join(' ').strip
+      us_data = query_response.find_first('.//us_market_data/common_us_data')
+      basic_data = query_response.find_first('.//us_market_data/common_us_data/basic_data')
       Lynr::Model::Vin.new(
         'year' => content(basic_data, './year'),
         'make' => content(basic_data, './make'),
-        'model' => model,
+        'model' => get_model(basic_data),
         'transmission' => content(us_data, './/transmission/type'),
         'fuel' => content(us_data, './/fuel_type'),
         'doors' => content(us_data, './/doors'),
         'drivetrain' => content(us_data, './/drive_type'),
-        'ext_color' => ext_colors.join(', '),
-        'int_color' => int_colors.join(', '),
+        'ext_color' => get_color_data(us_data, 'ext'),
+        'int_color' => get_color_data(us_data, 'int'),
         'number' => query_response['identifier'],
         'raw' => query_response.to_s
       )
+    end
+
+    # ## `#get_model(basic_data)`
+    #
+    # Take a `<basic_data />` node and get the model and trip information
+    # and join them together.
+    #
+    def get_model(basic_data)
+      [ content(basic_data, './model'),
+        content(basic_data, './trim')
+      ].join(' ').strip
+    end
+
+    # ## `#get_color_data(us_data, type)`
+    #
+    # Get color information out of `<us_data />` node for `type`, where
+    # `type` is 'ext' or 'int' and join them into one string.
+    #
+    def get_color_data(us_data, type)
+      contents(us_data, ".//#{type}erior_colors//generic_color_name").join(', ')
     end
 
   end
