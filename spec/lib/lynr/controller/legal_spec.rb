93b143437cf7e074fd9fd91ca4f0a8799c807faa
Bryan Swift <bryan@lynr.co>
2014-03-13 21:17:37 -0700

    Additional coverage for `Lynr::Controller::Legal`
    Make sure the request flow is tested.
    
    See #24

--- spec/lib/lynr/controller/legal_spec.rb
+++ spec/lib/lynr/controller/legal_spec.rb
@@ -53,6 +53,33 @@ describe Lynr::Controller::Legal do
 
     end
 
+    describe "route.call(req)" do
+
+      let(:response) { route.call(env) }
+
+      it "is an Array" do
+        expect(response).to be_instance_of(Array)
+      end
+
+      it "is a finished Rack::Response (length 3 array)" do
+        expect(response.length).to eq(3)
+      end
+
+      it "has headers in second position" do
+        expect(response[1]).to be_instance_of(Rack::Utils::HeaderHash)
+        expect(response[1].is_a?(Hash)).to be_true
+      end
+
+      it "has body in third position" do
+        expect(response[2]).to be_instance_of(Rack::BodyProxy)
+      end
+
+      it "is a 200 response" do
+        expect(response[0]).to eq(200)
+      end
+
+    end
+
   end
 
   shared_examples "invalid request" do
