8c38e797010038a9453a917f1f56a915ac052c64
Bryan Swift <bryan@lynr.co>
2014-03-25 11:58:08 -0700

    Make ModelHelper a shared context rather than a module

--- spec/support/model_helper.rb
+++ spec/support/model_helper.rb
@@ -1,21 +1,13 @@
 require './lib/lynr/model/dealership'
 require './lib/lynr/model/identity'
+require './lib/lynr/model/vehicle'
 
-module ModelHelper
+shared_context "spec/support/ModelHelper" do
 
-  def identity
-    return @identity unless @identity.nil?
-    @identity = Lynr::Model::Identity.new('bryan@lynr.co', 'this is a fake password')
-  end
-
-  def empty_dealership
-    return @empty_dealership unless @empty_dealership.nil?
-    @empty_dealership = Lynr::Model::Dealership.new({ 'identity' => identity, })
-  end
-
-  def saved_empty_dealership
-    return @saved_empty_dealership unless @saved_empty_dealership.nil?
-    @saved_empty_dealership = subject.dealer_dao.save(empty_dealership)
-  end
+  let(:identity) { Lynr::Model::Identity.new('bryan@lynr.co', 'this is a fake password') }
+  let(:empty_dealership) { Lynr::Model::Dealership.new({ 'identity' => identity, }) }
+  let(:saved_empty_dealership) { subject.dealer_dao.save(empty_dealership) }
+  let(:empty_vehicle) { Lynr::Model::Vehicle.new({ 'dealership' => saved_empty_dealership }) }
+  let(:saved_empty_vehicle) { subject.vehicle_dao.save(empty_vehicle) }
 
 end
