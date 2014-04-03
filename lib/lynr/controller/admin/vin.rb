f4b461448273da61f4cec53e2c4e74a6e4360fa7
Bryan Swift <bryan@lynr.co>
2014-04-02 17:49:00 -0700

    Create DataOne module/namespace and Api class
    See #207 - https://sprint.ly/product/16794/item/207
    
    Extract the logic for accessing the DataOne API so the places that use
    it can be decoupled and moved around.

--- lib/lynr/controller/admin/vin.rb
+++ lib/lynr/controller/admin/vin.rb
@@ -1,6 +1,8 @@
 require 'libxml'
 require 'rest-client'
 
+require './lib/data_one'
+
 require './lib/lynr'
 require './lib/lynr/controller/admin'
 require './lib/lynr/converter/data_one'
@@ -29,7 +31,7 @@ module Lynr; module Controller;
     #
     def search(req)
       posted['dealership'] = @dealership
-      query_response = fetch(posted['vin'].upcase)
+      query_response = DataOne::Api.fetch(posted['vin'].upcase)
       if query_response.nil?
         @subsection = 'vehicle-add'
         @title = 'Add Vehicle'
@@ -37,55 +39,11 @@ module Lynr; module Controller;
         @errors = { 'vin' => 'Vin not found.' }
         render 'admin/vehicle/add.erb'
       else
-        vehicle = vehicle_dao.save(xml_to_vehicle(query_response).set({
+        vehicle = save_vehicle(xml_to_vehicle(query_response).set({
           'dealership' => @dealership
         }))
-        Lynr.producer('job').publish(Lynr::Queue::IndexVehicleJob.new(vehicle))
         redirect "/admin/#{@dealership.slug}/#{vehicle.slug}/edit"
       end
-    end
-
-    # ## `AdminVin#fetch(vin)`
-    #
-    # Retrieve vehicle data from the the DataOne back end and return a
-    # `LibXML::XML::Node` with the `<query_response />` element from the data.
-    #
-    def fetch(vin)
-      if File.exists?("spec/data/#{vin}.xml")
-        doc = LibXML::XML::Document.file("spec/data/#{vin}.xml")
-      elsif Lynr.cache.include?(vin)
-        doc = LibXML::XML::Document.string(Lynr.cache.read(vin))
-      elsif Lynr.features.dataone_decode
-        response = fetch_dataone(vin).tap { |resp| Lynr.cache.write(vin, resp) }
-        doc = LibXML::XML::Document.string(response)
-      else
-        doc = LibXML::XML::Document.new
-        node = LibXML::XML::Node.new 'query_response'
-        doc.root = node
-      end
-      doc.find_first("//query_response[@identifier='#{vin}']")
-    end
-
-    def fetch_dataone(vin)
-      config = Lynr.config('app').vin.dataone
-      url = config.url
-      data = {
-        authorization_code: config.auth_code,
-        client_id:          config.client_id,
-        decoder_query:      dataone_xml_query(vin),
-      }
-      Lynr.metrics.time('time.service:dataone.fetch') do
-        RestClient.post url, data
-      end
-    end
-
-    # ## `AdminVin#dataone_xml_query(vin)`
-    #
-    # Helper method to render query template with the appropriate `vin` data.
-    #
-    def dataone_xml_query(vin)
-      path = ::File.join(Lynr.root, 'views/admin/vehicle/dataone_request')
-      Sly::View::Erb.new(path, data: { vin: vin }).result
     end
 
   end
