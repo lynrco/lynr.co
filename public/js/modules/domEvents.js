b2dbb44f5c8a85bdfddf451a53556a4eaf90903e
Bryan Swift <bryan@lynr.co>
2014-04-09 15:11:51 -0700

    Guard against adding/removing events on undefined elements
    See #235 - https://sprint.ly/product/16794/item/235

--- public/js/modules/domEvents.js
+++ public/js/modules/domEvents.js
@@ -8,9 +8,9 @@ define(function() {
 
   // Functions taken from http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
   function addEvent(obj, type, fn) {
-    if (obj.addEventListener) {
+    if (obj && obj.addEventListener) {
       obj.addEventListener(type, fn, false);
-    } else if (obj.attachEvent) {
+    } else if (obj && obj.attachEvent) {
       obj["e" + type + fn] = fn;
       obj[type + fn] = function() { obj["e" + type + fn](window.event); };
       obj.attachEvent("on" + type, obj[type + fn]);
@@ -18,14 +18,14 @@ define(function() {
   }
 
   function removeEvent(obj, type, fn) {
-    if (obj.removeEventListener) {
+    if (obj && obj.removeEventListener) {
       obj.removeEventListener(type, fn, false);
-    } else if (obj.detachEvent) {
+    } else if (obj && obj.detachEvent) {
       obj.detachEvent("on" + type, obj[type + fn]);
       obj[type + fn] = null;
       obj["e" + type + fn] = null;
     } else if (console && console.warn) {
-      console.warn('no way to remove event from', node, name);
+      console.warn('no way to remove event from', obj, type);
     }
   }
 
