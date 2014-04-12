92f368f20d2a5ee1b432bd6c7e16de667deaff0e
Bryan Swift <bryan@lynr.co>
2014-04-12 14:58:59 -0700

    Create a way to add event handlers to Consumer
    See #234 - https://sprint.ly/product/16794/item/234
    
    Emitting events doesn't do much good without something consuming them.

--- lib/lynr/events/consumer.rb
+++ lib/lynr/events/consumer.rb
@@ -1,3 +1,5 @@
+require 'yajl/json_gem'
+
 require './lib/lynr'
 require './lib/lynr/events'
 require './lib/lynr/queue'
@@ -18,6 +20,24 @@ module Lynr
     #
     def initialize
       super("#{Lynr.env}.events")
+      @backend = {}
+      @semaphore = Mutex.new
+    end
+
+    # ## `Events::Consumer#add(type, handler)`
+    #
+    # Add an event handler to the `Consumer` instance. `handler` is used
+    # to process events of `type` when they are received from `#consumer`.
+    #
+    def add(type, handler)
+      if !handler.respond_to?(:call)
+        raise ArgumentError.new("Lynr::Events handler must respond_to?(:call)")
+      end
+      @semaphore.synchronize {
+        subscribers = @backend.fetch(type, [])
+        @backend.store(type, subscribers.dup << handler)
+      }
+      self
     end
 
     # ## `Events::Consumer#consumer`
@@ -40,12 +60,29 @@ module Lynr
       log.warn("type=parse.error payload=#{paylaod}")
     end
 
+    # ## `Events::Consumer#handlers_for(type)`
+    #
+    # Retrieves the handlers for the given `type` of event. This is
+    # used to retrieve the current set of handlers used to process a
+    # received event.
+    #
+    def handlers_for(type)
+      @semaphore.synchronize {
+        @backend.fetch(type, [])
+      }
+    end
+
     # ## `Events::Consumer#process(delivery_info, metadata, payload)`
     #
     # Defines what to do when a messages is received from `#consumer`.
     #
     def process(delivery_info, metadata, payload)
       event = deserialize(delivery_info, metadata, payload)
+      handlers = handlers_for(event['type'])
+      results = handlers.map do |handler|
+        handler.call(event)
+        log.info("type=processed.handler handler=#{handler.id}")
+      end
       log.info("type=processed payload=#{event.to_json}")
     end
 
