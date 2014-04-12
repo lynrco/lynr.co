1130a9053761793645a60b28f1ff762d9a4bc34d
Bryan Swift <bryan@lynr.co>
2014-04-12 15:00:20 -0700

    `Lynr::Events::Handler` is to be the parent of event handlers
    See #234 - https://sprint.ly/product/16794/item/234
    
    It is also used to construct the event handlers from configuration data.

---
+++ lib/lynr/events/handler.rb
@@ -0,0 +1,39 @@
+require './lib/lynr'
+require './lib/lynr/events'
+require './lib/lynr/persist/dealership_dao'
+
+module Lynr
+
+  # # `Lynr::Events::Handler`
+  #
+  # Meant to be parent class for Event handlers. As such it implements
+  # common functionality for them.
+  #
+  class Events::Handler
+
+    # ## `Events::Handler#dealership_dao`
+    #
+    # Get an instance of `Lynr::Persist::DealershipDao`
+    #
+    def dealership_dao
+      @dealership_dao ||= Lynr::Persist::DealershipDao.new
+    end
+
+    # ## `Events::Handler.from(config)`
+    #
+    # Takes a `config` `Hash` containing a 'type' key. 'type' is used to
+    # determine the class by incrementally calling `.const_get` on the
+    # scope. Once the type has been resolved `config` is passed to the
+    # constructor.
+    #
+    def self.from(config)
+      class_name = config.delete('type')
+      type = class_name.split('::').inject(Kernel) do |scope, name|
+        scope.const_get(name)
+      end
+      type.new(config)
+    end
+
+  end
+
+end
