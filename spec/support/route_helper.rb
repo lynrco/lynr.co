6d38ebf97b524b309a9c296d714ecaf63c263f92
Bryan Swift <bryan@lynr.co>
2014-04-15 23:11:34 -0700

    Make response fields available for testing
    See #197 - https://sprint.ly/product/16794/item/197
    
    In order to check elements are available in the DOM this will be
    helpful.

--- spec/support/route_helper.rb
+++ spec/support/route_helper.rb
@@ -13,6 +13,14 @@ shared_context "spec/support/RouteHelper" do
   let(:env) { env_for(uri) }
   let(:req) { Sly::Request.new(env, route.path_regex) }
   let(:response) { route.call(env) }
+  let(:response_headers) { response[1] }
+  let(:response_body) {
+    response[2].body.reduce("") { |m,d| m + d }
+  }
+  let(:response_body_document) {
+    require 'nokogiri'
+    Nokogiri::HTML::Document.parse(response_body)
+  }
 
   def env_for(uri)
     Rack::MockRequest.env_for("https://#{domain}#{uri}", env_opts)
