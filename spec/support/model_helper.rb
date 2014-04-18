6930db77407795a29bea8432d6c06bd59a30e723
Bryan Swift <bryan@lynr.co>
2014-04-18 09:58:06 -0700

    Include customer information in the empty dealership
    See #197 - https://sprint.ly/product/16794/item/197
    
    Some '/admin/:slug/*' pages need a customer_id attached to the
    dealership, specifically billing pages, so include it by default.

--- spec/support/model_helper.rb
+++ spec/support/model_helper.rb
@@ -5,9 +5,17 @@ require './lib/lynr/model/vehicle'
 shared_context "spec/support/ModelHelper" do
 
   let(:identity) { Lynr::Model::Identity.new('bryan@lynr.co', 'this is a fake password') }
-  let(:empty_dealership) { Lynr::Model::Dealership.new({ 'identity' => identity, }) }
+  let(:empty_dealership) {
+    Lynr::Model::Dealership.new({ 'identity' => identity, 'customer_id' => mock_customer.id, })
+  }
   let(:saved_empty_dealership) { subject.dealer_dao.save(empty_dealership) }
   let(:empty_vehicle) { Lynr::Model::Vehicle.new({ 'dealership' => saved_empty_dealership }) }
   let(:saved_empty_vehicle) { subject.vehicle_dao.save(empty_vehicle) }
+  let(:mock_customer) {
+    Stripe::Customer.create({
+      email: 'johnny@appleseed.com',
+      card: StripeMock.generate_card_token(last4: "4242", exp_year: 2016),
+    })
+  }
 
 end
