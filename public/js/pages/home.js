833c82b2199f9bb3999be85f023749eb5a5347fc
Bryan Swift <bryan@lynr.co>
2014-04-17 23:04:45 -0700

    Initialize the demo signup form if it exists
    See #197 - https://sprint.ly/product/16794/item/197
    
    The homepage will no longer contain the marketing launch notification
    page so there is no need to handle that case any longer.

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -3,17 +3,25 @@ define(function(require) {
   var mixpanel = require('mixpanel');
   var evt = require('modules/dom-events');
 
+  function setupHome() {
+    var form = document.querySelector('form.signup-demo');
+    if (!form) { return; }
+
+    evt.on(form, 'submit', trackFormSubmit);
+    if (document.querySelector('#agree_terms')) { styleInputs(); }
+  }
+
+  function styleInputs() {
+    var styleCheckbox = require('modules/style-checkbox');
+    styleCheckbox(document.querySelector('#agree_terms'));
+  }
+
   function trackFormSubmit(e) {
-    mixpanel.track('signup.launch', {
-      description: 'Signed up for Launch Notification',
+    mixpanel.track('signup.demo', {
+      description: 'Signed up for Demo Account',
       url: window.location.pathname,
       domain: window.location.host || window.location.hostname
     });
-  }
-
-  function setupHome() {
-    var form = document.querySelector('form.signup');
-    evt.on(form, 'submit', trackFormSubmit);
   }
 
   return setupHome;
