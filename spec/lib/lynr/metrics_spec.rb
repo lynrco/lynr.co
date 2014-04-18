6e9f555f67404d143ee9d2d31e6f04d94133ab69
Bryan Swift <bryan@lynr.co>
2014-04-17 22:34:56 -0700

    Significant rework of `Lynr::Metrics`
    See #197 - https://sprint.ly/product/16794/item/197
    
    Many methods in `Lynr::Metrics` accepted a config object in order to
    test how the class reacted when some of the librato configuration was
    missing. This was necessary before the ConfigHelper shared_context. This
    refactoring still checks for partial configuration but handles it
    better (by using a dummy queue when not enabled). Additionally, the
    specs are now more consistent with the usage pattern in the application
    rather than being artificial because of the needs of testing.

--- spec/lib/lynr/metrics_spec.rb
+++ spec/lib/lynr/metrics_spec.rb
@@ -8,132 +8,110 @@ describe Lynr::Metrics do
 
   include_context "spec/support/ConfigHelper"
 
-  let(:librato_config) {
+  subject(:metrics) { Lynr::Metrics.new }
+  subject(:queue) { metrics.queue }
+  subject(:client) { queue.client }
+
+  def librato_config
     {
-      'user'   => 'bryan@lynr.co',
-      'token'  => "I'm not real",
-      'source' => 'lynr.specs',
+      'enabled' => true,
+      'user'    => 'bryan@lynr.co',
+      'token'   => "I'm not real",
+      'source'  => 'lynr.specs',
     }
-  }
+  end
 
-  describe "#add" do
+  describe "EMPTY_QUEUE" do
+    subject(:queue) { Lynr::Metrics::EMPTY_QUEUE }
+    it "should have nil client" do expect(queue.client).to be_nil end
+    it "should be empty" do expect(queue.empty?).to be_true end
+  end
 
-    before(:each) do
-      Librato::Metrics::Queue.any_instance.stub(:add) do |metrics|
-        self
+  context "with full librato config" do
+
+    before(:all) do
+      stub_config('app', 'librato' => {
+        'enabled' => true,
+        'user'    => 'bryan@lynr.co',
+        'token'   => "I'm not real",
+        'source'  => 'lynr.specs',
+      })
+    end
+
+    describe "#configured?" do
+
+      it "is true when given config with user, token and source" do
+        expect(metrics.configured?).to be_true
+      end
+
+    end
+
+    describe "#queue" do
+
+      it "is an instance of Librato::Metrics::Queue" do
+        expect(queue).to be_instance_of(Librato::Metrics::Queue)
+      end
+
+      # NOTE: Dangerous spec, testing implementation details of a dependency,
+      # because this value isn't exposed
+      it "has an autosubmit_count set" do
+        expect(queue.instance_variable_get(:@autosubmit_count)).to eq(15)
+      end
+
+      # NOTE: Dangerous spec, testing implementation details of a dependency,
+      # because this value isn't exposed
+      it "has an autosubmit_interval set" do
+        expect(queue.instance_variable_get(:@autosubmit_interval)).to eq(90)
+      end
+
+      it "has an instance of Librato::Metrics::Client" do
+        expect(queue.client).to be_instance_of(Librato::Metrics::Client)
+      end
+
+      it "has a client with email" do
+        expect(client.email).to eq(librato_config['user'])
+      end
+
+      it "has a client with an api_key" do
+        expect(client.api_key).to eq(librato_config['token'])
+      end
+
+    end
+
+  end
+
+  ['user', 'token', 'source'].each do |key|
+
+    context "with partial config -- #{key} missing" do
+      before(:all) do
+        stub_config('app', 'librato' => librato_config.delete_if { |k,v| k == key })
+      end
+      describe "#configured?" do
+        it { expect(metrics.configured?).to be_false }
+      end
+      describe "#enabled?" do
+        it { expect(metrics.enabled?).to be_true }
+      end
+      describe "#queue" do
+        it "should be EMPTY_QUEUE" do expect(queue).to eq(Lynr::Metrics::EMPTY_QUEUE) end
       end
     end
 
   end
 
-  describe "#configured?" do
-
-    it "is true when given config with user, token and source" do
-      config = Lynr::Config.new(nil, 'neverland', librato_config)
-      expect(subject.configured?(config)).to be_true
+  context "with no librato config" do
+    before(:all) do
+      stub_config('app', 'librato' => nil)
     end
-
-    ['user', 'token', 'source'].each do |key|
-
-      let(:config) { Lynr::Config.new(nil, 'neverland', librato) }
-
-      it "is false when given config without #{key}" do
-        librato = librato_config.delete_if { |k,v| k == key }
-        config = Lynr::Config.new(nil, 'neverland', librato)
-        expect(subject.configured?(config)).to be_false
-      end
-
-      it "is false when given config with only #{key}" do
-        librato = librato_config.delete_if { |k,v| k != key }
-        config = Lynr::Config.new(nil, 'neverland', librato)
-        expect(subject.configured?(config)).to be_false
-      end
-
+    describe "#configured?" do
+      it { expect(metrics.configured?).to be_false }
     end
-
-  end
-
-  describe "#queue" do
-
-    let(:queue) { subject.queue(librato_config) }
-    let(:client) { queue.client }
-
-    it "returns an instance of Librato::Metrics::Queue" do
-      expect(queue).to be_instance_of(Librato::Metrics::Queue)
+    describe "#enabled?" do
+      it { expect(metrics.enabled?).to be_false }
     end
-
-    # NOTE: Dangerous spec, testing implementation details of a dependency,
-    # because this value isn't exposed
-    it "has an autosubmit_count set" do
-      expect(queue.instance_variable_get(:@autosubmit_count)).to eq(15)
+    describe "#queue" do
+      it "should be EMPTY_QUEUE" do expect(queue).to eq(Lynr::Metrics::EMPTY_QUEUE) end
     end
-
-    # NOTE: Dangerous spec, testing implementation details of a dependency,
-    # because this value isn't exposed
-    it "has an autosubmit_interval set" do
-      expect(queue.instance_variable_get(:@autosubmit_interval)).to eq(90)
-    end
-
-    it "has a client" do
-      expect(queue.client).to be
-    end
-
-    it "has an instance of Librato::Metrics::Client" do
-      expect(queue.client).to be_instance_of(Librato::Metrics::Client)
-    end
-
-    it "has a client with email" do
-      expect(client.email).to eq(librato_config['user'])
-    end
-
-    it "has a client with an api_key" do
-      expect(client.api_key).to eq(librato_config['token'])
-    end
-
-    context "when #configured? is false" do
-
-      let(:librato_config) {
-        {
-          'user'   => 'bryan@lynr.co',
-          'token'  => "I'm not real",
-        }
-      }
-
-      it "has a client without email" do
-        expect(client.email).to be_nil
-      end
-
-      it "has a client without api_key" do
-        expect(client.api_key).to be_nil
-      end
-
-    end
-
-    context "with no explicit config" do
-
-      before(:each) do
-        stub_config('app', {
-          'librato' => {
-            'user' => 'foo@bar.com',
-            'token' => 'madeup',
-            'source' => 'lynr-co-spec',
-          }
-        })
-      end
-
-      let(:queue) { subject.queue }
-      let(:config) { Lynr.config('app') }
-
-      it "has a client with email from Lynr.config" do
-        expect(client.email).to eq(config.librato.user)
-      end
-
-      it "has a client with api_key from Lynr.config" do
-        expect(client.api_key).to eq(config.librato.token)
-      end
-
-    end
-
   end
 
 end
