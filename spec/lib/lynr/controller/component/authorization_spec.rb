664f550dce6cef1698f66139a290bc86cfc1e6d1
Bryan Swift <bryan@lynr.co>
2014-04-21 15:50:54 -0700

    Authorization controller component
    See #197 - https://sprint.ly/product/16794/item/197
    
    Extracts logic for authorization out of the admin parent controller.

---
+++ spec/lib/lynr/controller/component/authorization_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec/autorun'
+require './spec/spec_helper'
+
+require './lib/lynr/controller/component/authorization'
+require './lib/lynr/persist/dealership_dao'
+
+describe Lynr::Controller::Authorization do
+
+  include_context "spec/support/ModelHelper"
+  include_context "spec/support/RouteHelper"
+
+  class Dummy
+    include Lynr::Controller::Authorization
+    def dealer_dao
+      @dealer_dao ||= Lynr::Persist::DealershipDao.new
+    end
+  end
+
+  subject(:controller) { Dummy.new }
+
+  describe "#authorized?" do
+    let(:dealership) { saved_empty_dealership }
+    it "should be true if dealership.id in role" do
+      role = "admin:#{saved_empty_dealership.id}"
+      expect(controller.authorized?(role, saved_empty_dealership)).to be_true
+    end
+    it "should be faulse if dealership.id not in role" do
+      role = "admin:#{BSON::ObjectId.from_time(Time.now)}"
+      expect(controller.authorized?(role, dealership)).to be_false
+    end
+  end
+
+end
