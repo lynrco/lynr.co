cf403cf4c88451ee90e804b3f5c84cba8b1f1d0d
Bryan Swift <bryan@lynr.co>
2014-04-26 10:06:20 -0700

    Add POST specs for signin
    See #261 - https://sprint.ly/product/16794/item/261

--- spec/lib/lynr/controller/auth/signin_spec.rb
+++ spec/lib/lynr/controller/auth/signin_spec.rb
@@ -16,10 +16,10 @@ describe Lynr::Controller::Auth::Signin do
     Lynr::Controller::Auth::Signin.new
   }
 
-  let(:path) { '/signup' }
-  let(:uri) { "/signup" }
+  let(:path) { '/signin' }
+  let(:uri) { "/signin" }
 
-  context "GET /signup" do
+  context "GET /signin" do
     let(:route_method) { [:get_signin, 'GET'] }
     it_behaves_like "Lynr::Controller::Base#valid_request"
     it { expect(response_body_document).to have_element('form.signin') }
@@ -27,7 +27,7 @@ describe Lynr::Controller::Auth::Signin do
     context "with features.demo" do
       include_context "features.demo=true"
       it_behaves_like "Lynr::Controller::Base#valid_request"
-      it { expect(response_body_document).to have_element('form.signin-demo') }
+      it { expect(response_body_document).to have_element('form.signup-demo') }
     end
 
     context "with signed-in session" do
@@ -43,6 +43,53 @@ describe Lynr::Controller::Auth::Signin do
       it_behaves_like "Lynr::Controller::Base#valid_request", 302 do
         it { expect(response_headers).to include('Location') }
         it { expect(response_headers['Location']).to eq("/admin/#{saved_empty_dealership.id}") }
+      end
+    end
+  end
+
+  context "POST /signin" do
+    let(:route_method) { [:post_signin, 'POST'] }
+    let(:posted) do
+      { 'email' => saved_empty_dealership.identity.email, 'password' => 'this is a fake password', }
+    end
+    let(:env_opts) do { params: posted } end
+
+    let(:response_dealership) {
+      location = response_headers['Location']
+      dealership_id = location.match(%r(/admin/(?<id>.*)$))['id']
+      controller.dealer_dao.get(dealership_id)
+    }
+
+    context "with valid credentials" do
+      it_behaves_like "Lynr::Controller::Base#valid_request", 302 do
+        it "creates dealership with active subscription" do
+          expect(response_dealership.identity.email).to eq(posted['email'])
+        end
+      end
+    end
+
+    context "with incorrect password" do
+      let(:posted) do super().merge({ 'password' => 'invalid password' }) end
+      it_behaves_like "Lynr::Controller::Base#valid_request" do
+        it { expect(response_body_document).to have_element('form.signin') }
+        it { expect(response_body_document).to_not have_element('form.signup-demo') }
+      end
+    end
+
+    context "with features.demo" do
+      include_context "features.demo=true"
+      let(:posted) do { 'email' => saved_demo_dealership.identity.email } end
+      it_behaves_like "Lynr::Controller::Base#valid_request", 302 do
+        it "creates dealership with active subscription" do
+          expect(response_dealership.identity.email).to eq(posted['email'])
+        end
+      end
+
+      context "with email of non-demo account" do
+        let(:posted) do { 'email' => saved_empty_dealership.identity.email } end
+        it_behaves_like "Lynr::Controller::Base#valid_request" do
+          it { expect(response_body_document).to have_element('form.signup-demo') }
+        end
       end
     end
   end
