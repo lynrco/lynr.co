4d6e50fd977aec22eace313296fbf664d3d7f374
Bryan Swift <bryan@lynr.co>
2014-04-26 10:06:32 -0700

    Always setup the agree checkbox on auth pages
    See #261 - https://sprint.ly/product/16794/item/261

--- public/js/pages/auth.js
+++ public/js/pages/auth.js
@@ -1,15 +1,19 @@
 define(function(require) {
 
   var api = {
+    _init: initAgree,
     signup: initSignup
   };
 
+  function initAgree() {
+    var styleCheckbox = require('modules/style-checkbox');
+    styleCheckbox(document.querySelector('#agree_terms'));
+  }
+
   function initSignup() {
     var setupStripeForm = require('modules/stripe')
-    var styleCheckbox = require('modules/style-checkbox');
 
     setupStripeForm(document.querySelector('form.signup'));
-    styleCheckbox(document.querySelector('#agree_terms'));
   }
 
   return api;
