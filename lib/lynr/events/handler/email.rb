d0ba93e04482a5b429b449812216d0af3cc3d06c
Bryan Swift <bryan@lynr.co>
2014-04-30 18:35:04 -0700

    Create method to convert live domain to demo domain
    See #269 - https://sprint.ly/product/16794/item/269
    
    In order to not require separate queue processing infrastructure for
    each instance of the Lynr web application it is necessary to translate
    from the live domain to the demo domain when events are of the demo
    type. This feels wrong but I don't see a better way to handle the
    situation without either:
    
    1. having separate queue processors with config matching application
       configuration
    2. passing the whole application configuration as part of the event
    
    The first solution is the 'right' way but would require expensive,
    mostly idle, heroku dynos for each demo site (idle particularly for the
    staging site) or a new deployment infrastructure such that the
    application (web or queue processor) could run on
    self-hosted/self-administered hardware. And while I think going down the
    path of self-hosted will happen I'm not prepared to commit to the
    infrastructure and planning necessary to accomplish it yet. This
    statement is particularly ironic when considering I spent two days
    working on a 'better' solution to the problem, which presumably would
    have been enough time to get a solid start on a different self-hosted
    infrastructure setup.

--- lib/lynr/events/handler/email.rb
+++ lib/lynr/events/handler/email.rb
@@ -35,6 +35,38 @@ module Lynr
       success
     end
 
+    # ## `Handler::Email#domain(event)`
+    #
+    # Internal: Get the domain to send to `Lynr::Queue::EmailJob`s
+    # produced by this event handler. `domain` is dependent on
+    # `event[:type]` and `Lynr.config('app').domain`. If domain from
+    # application configuration. This method knows about the live and
+    # staging environments/domains and no others.
+    #
+    # NOTE: This feels wrong. The application shouldn't have to make
+    # inferences based on configuration values but I can't figure out a
+    # cleaner way to get this bit of config from the event producer
+    # (demo site) to the event processor (live site) without having an
+    # event processor unique to each event producer. - BJS 2014-04-30
+    #
+    # * event - `Hash` of event data to be handled by this event handler.
+    #
+    # Returns String to be used as the base domain in the email template
+    # when sent.
+    #
+    def domain(event)
+      domain = Lynr.config('app').domain
+      if event[:type].match(/\.demo\Z/)
+        case domain
+        when %r(stage\.herokuapp\.com$) then domain.gsub(/\.heroku/, '-demo.heroku')
+        when %r(\Awww\.lynr\.co\Z) then domain.gsub(/www/, 'demo')
+        else domain
+        end
+      else
+        domain
+      end
+    end
+
     # ## `Events::Handler::Email#id`
     #
     # Identifier for a configured `Email` handler. The handler `#id`
@@ -49,11 +81,11 @@ module Lynr
     # Get/create mail template properties from `event`.
     #
     def mail_defaults(event)
-      app_config = Lynr.config('app')
       {
         to: to(event),
-        base_url: "https://#{app_config.domain}",
-        support_email: app_config.support_email,
+        base_url: "https://#{domain(event)}",
+        dealership: dealership(event),
+        support_email: Lynr.config('app').support_email,
       }
     end
 
