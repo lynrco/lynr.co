9a37a5bf2ed294653178b89d4c79a6f00885729e
Bryan Swift <bryan@lynr.co>
2014-05-04 11:33:44 -0700

    Create a Promise based fastdom API
    See #243 - https://sprint.ly/product/16794/item/243

---
+++ public/js/modules/fastdomp.js
@@ -0,0 +1,24 @@
+define(
+  ['fastdom', 'promise'],
+  function(fastdom, Promise) {
+
+    function read(fn) {
+      return new Promise(function(resolve, reject) {
+        fastdom.read(fn.bind(this, resolve, reject));
+      });
+    }
+
+    function write(fn) {
+      return new Promise(function(resolve, reject) {
+        fastdom.write(fn.bind(this, resolve, reject));
+      });
+    }
+
+    return {
+      o: fastdom,
+      read: read,
+      write: write
+    };
+
+  }
+);
